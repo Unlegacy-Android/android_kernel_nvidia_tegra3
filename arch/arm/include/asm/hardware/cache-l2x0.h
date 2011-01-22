9770929b95a72ff538920673258fafe591816924
Colin Cross <ccross@android.com>
2011-01-22 00:37:09 -0800

    ARM: mm: cache-l2x0: Add support for re-enabling l2x0
    Remove __init annotation from l2x0_init so it can be used to
    reinitialize the l2x0 after it has been reset during suspend.
    
    Only print the init messages the first time l2x0_init is called.
    
    Add l2x0_enable to re-enable the l2x0 after l2x0_disable if
    the l2x0 was not reset.
    
    l2x0_disable cannot use writel, as writel calls wmb(), and wmb()
    may call outer_cache_sync, which takes the same spinlock as
    l2x0_disable.
    
    Change-Id: Iaddedb4f582c7eeaef3cbe2a1e463787f0f809a4
    Signed-off-by: Colin Cross <ccross@android.com>
    
    Rebase-Id: R287097a72fda68464dc312ff0f7a775919d6d81d

--- arch/arm/include/asm/hardware/cache-l2x0.h
+++ arch/arm/include/asm/hardware/cache-l2x0.h
@@ -104,6 +104,8 @@
 
 #ifndef __ASSEMBLY__
 extern void __init l2x0_init(void __iomem *base, __u32 aux_val, __u32 aux_mask);
+extern void l2x0_init(void __iomem *base, __u32 aux_val, __u32 aux_mask);
+extern void l2x0_enable(void);
 #if defined(CONFIG_CACHE_L2X0) && defined(CONFIG_OF)
 extern int l2x0_of_init(__u32 aux_val, __u32 aux_mask);
 #else
