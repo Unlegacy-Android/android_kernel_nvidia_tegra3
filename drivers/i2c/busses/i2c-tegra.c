c317acd59ad129e8e22333fbffa0a7a1d9a28f67
Laxman Dewangan <ldewangan@nvidia.com>
2011-04-27 19:11:08 +0530

    i2c: clock: tegra: Removing unnecessary clock entry
    The clock table have two different entry for a given i2c controller.
    The second entry is not required and hence removing from table.
    Also removing the reference of this entry from driver.
    
    Original-Change-Id: Iab5f79bf5d1fea9363ef524258fb3ec262547691
    Reviewed-on: http://git-master/r/29629
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Tested-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Daniel Willemsen <dwillemsen@nvidia.com>
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Narendra Damahe <ndamahe@nvidia.com>
    
    Rebase-Id: R0265f605ec00f464eb9ec8d5ffe840a642592fab

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -141,7 +141,6 @@ struct tegra_i2c_bus {
 struct tegra_i2c_dev {
 	struct device *dev;
 	struct clk *clk;
-	struct clk *i2c_clk;
 	struct resource *iomem;
 	struct rt_mutex dev_lock;
 	void __iomem *base;
@@ -694,7 +693,6 @@ static int __devinit tegra_i2c_probe(struct platform_device *pdev)
 	struct resource *res;
 	struct resource *iomem;
 	struct clk *clk;
-	struct clk *i2c_clk;
 	const unsigned int *prop;
 	void __iomem *base;
 	int irq;
@@ -747,23 +745,15 @@ static int __devinit tegra_i2c_probe(struct platform_device *pdev)
 		goto err_release_region;
 	}
 
-	i2c_clk = clk_get(&pdev->dev, "i2c");
-	if (IS_ERR(i2c_clk)) {
-		dev_err(&pdev->dev, "missing bus clock");
-		ret = PTR_ERR(i2c_clk);
-		goto err_clk_put;
-	}
-
 	i2c_dev = kzalloc(sizeof(struct tegra_i2c_dev) +
 			  (nbus-1) * sizeof(struct tegra_i2c_bus), GFP_KERNEL);
 	if (!i2c_dev) {
 		ret = -ENOMEM;
-		goto err_i2c_clk_put;
+		goto err_clk_put;
 	}
 
 	i2c_dev->base = base;
 	i2c_dev->clk = clk;
-	i2c_dev->i2c_clk = i2c_clk;
 	i2c_dev->iomem = iomem;
 	i2c_dev->irq = irq;
 	i2c_dev->cont_id = pdev->id;
@@ -799,7 +789,6 @@ static int __devinit tegra_i2c_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, i2c_dev);
 
-	clk_enable(i2c_dev->i2c_clk);
 	if (i2c_dev->is_clkon_always)
 		clk_enable(i2c_dev->clk);
 
@@ -861,8 +850,6 @@ err_del_bus:
 	free_irq(i2c_dev->irq, i2c_dev);
 err_free:
 	kfree(i2c_dev);
-err_i2c_clk_put:
-	clk_put(i2c_clk);
 err_clk_put:
 	clk_put(clk);
 err_release_region:
@@ -882,7 +869,6 @@ static int __devexit tegra_i2c_remove(struct platform_device *pdev)
 		clk_disable(i2c_dev->clk);
 
 	free_irq(i2c_dev->irq, i2c_dev);
-	clk_put(i2c_dev->i2c_clk);
 	clk_put(i2c_dev->clk);
 	release_mem_region(i2c_dev->iomem->start,
 		resource_size(i2c_dev->iomem));
