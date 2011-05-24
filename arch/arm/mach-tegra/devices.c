bedafdc823b546a2a1fe98fa0110f908f5955160
Scott Williams <scwilliams@nvidia.com>
2011-05-23 22:08:42 -0700

    arm: tegra: Clean up SOC conditionals
    Change SOC conditionals to make them more forward-looking.
    
    Original-Change-Id: Ib60db4e690c2f396afdec962616d735548b5a8a9
    Reviewed-on: http://git-master/r/32706
    Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
    Tested-by: Niket Sirsi <nsirsi@nvidia.com>
    
    Rebase-Id: R77c675a1995116098b58f1f775bc7c3cc8722998

--- arch/arm/mach-tegra/devices.c
+++ arch/arm/mach-tegra/devices.c
@@ -165,7 +165,7 @@ static struct resource i2c_resource3[] = {
 	},
 };
 
-#if defined(CONFIG_ARCH_TEGRA_2x_SOC)
+#ifdef CONFIG_ARCH_TEGRA_2x_SOC
 static struct resource i2c_resource4[] = {
 	[0] = {
 		.start	= INT_DVC,
@@ -179,7 +179,7 @@ static struct resource i2c_resource4[] = {
 	},
 };
 
-#elif defined(CONFIG_ARCH_TEGRA_3x_SOC)
+#else
 static struct resource i2c_resource4[] = {
 	[0] = {
 		.start  = INT_I2C4,
@@ -263,7 +263,7 @@ struct platform_device tegra_i2c_device4 = {
 	},
 };
 
-#ifdef CONFIG_ARCH_TEGRA_3x_SOC
+#ifndef CONFIG_ARCH_TEGRA_2x_SOC
 struct platform_device tegra_i2c_device5 = {
 	.name		= "tegra-i2c",
 	.id		= 4,
@@ -326,7 +326,7 @@ static struct resource spi_resource4[] = {
 		.flags	= IORESOURCE_MEM,
 	},
 };
-#if defined(CONFIG_ARCH_TEGRA_3x_SOC)
+#ifndef CONFIG_ARCH_TEGRA_2x_SOC
 static struct resource spi_resource5[] = {
 	[0] = {
 		.start  = INT_SPI_5,
@@ -393,7 +393,7 @@ struct platform_device tegra_spi_device4 = {
 		.coherent_dma_mask	= 0xffffffff,
 	},
 };
-#if defined(CONFIG_ARCH_TEGRA_3x_SOC)
+#ifndef CONFIG_ARCH_TEGRA_2x_SOC
 struct platform_device tegra_spi_device5 = {
 	.name           = "spi_tegra",
 	.id             = 4,
@@ -454,7 +454,7 @@ struct platform_device tegra_spi_slave_device4 = {
 		.coherent_dma_mask      = 0xffffffff,
 	},
 };
-#if defined(CONFIG_ARCH_TEGRA_3x_SOC)
+#ifndef CONFIG_ARCH_TEGRA_2x_SOC
 struct platform_device tegra_spi_slave_device5 = {
 	.name           = "spi_slave_tegra",
 	.id             = 4,
@@ -671,7 +671,7 @@ static struct resource tegra_pmu_resources[] = {
 		.end	= INT_CPU1_PMU_INTR,
 		.flags	= IORESOURCE_IRQ,
 	},
-#if defined(CONFIG_ARCH_TEGRA_3x_SOC)
+#ifndef CONFIG_ARCH_TEGRA_2x_SOC
 	[2] = {
 		.start	= INT_CPU2_PMU_INTR,
 		.end	= INT_CPU2_PMU_INTR,
@@ -807,7 +807,7 @@ struct platform_device tegra_uarte_device = {
 	},
 };
 
-#if defined(CONFIG_ARCH_TEGRA_2x_SOC)
+#ifdef CONFIG_ARCH_TEGRA_2x_SOC
 static struct resource i2s_resource1[] = {
 	[0] = {
 		.start	= INT_I2S1,
@@ -882,7 +882,7 @@ static struct resource spdif_resource[] = {
 	}
 };
 
-#elif defined(CONFIG_ARCH_TEGRA_3x_SOC)
+#else
 static struct resource audio_resource[] = {
 	[0] = {
 		.start	= TEGRA_AUDIO_CLUSTER_BASE,
@@ -1145,7 +1145,7 @@ static struct resource tegra_wdt_resources[] = {
 		.flags	= IORESOURCE_IRQ,
 	},
 };
-#elif defined(CONFIG_ARCH_TEGRA_3x_SOC)
+#else
 static struct resource tegra_wdt_resources[] = {
 	[0] = {
 		.start	= TEGRA_WDT0_BASE,
