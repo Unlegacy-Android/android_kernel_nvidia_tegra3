920d8c2a7f73de4de5fbd805f2c6442484581ae5
Bo Yan <byan@nvidia.com>
2011-08-11 15:03:08 -0700

    Revert "ARM: tegra: flush serial console earlier"
    This reverts commit 974dba4b42ba8663e0b1534975622634a3b1f16f.
    
    This function should be safe to call after all other CPUs
    have been stopped. It is better to call it after reboot callback
    functions have been called, so that any extra prints can be
    flushed.
    
    Conflicts:
    
    	arch/arm/mach-tegra/common.c
    
    Original-Change-Id: I2e2e07b78f2773a5e26cd893d955bdf9c678c3f0
    Reviewed-on: http://git-master/r/47339
    Reviewed-by: Bo Yan <byan@nvidia.com>
    Tested-by: Bo Yan <byan@nvidia.com>
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    
    Rebase-Id: Rc7dff9d9fb92f882334d2d771c674ad9cd1c6c72

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -26,9 +26,6 @@
 #include <linux/of_irq.h>
 #include <linux/highmem.h>
 #include <linux/memblock.h>
-#include <linux/notifier.h>
-#include <linux/reboot.h>
-#include <linux/mqueue.h>
 #include <linux/bitops.h>
 
 #include <asm/hardware/cache-l2x0.h>
@@ -312,17 +309,16 @@ static void __init tegra_init_ahb_gizmo_settings(void)
 
 static bool console_flushed;
 
-static int tegra_pm_flush_console(struct notifier_block *this,
-	unsigned long code, void *unused)
+static void tegra_pm_flush_console(void)
 {
 	if (console_flushed)
-		return NOTIFY_NONE;
+		return;
 	console_flushed = true;
 
 	pr_emerg("Restarting %s\n", linux_banner);
 	if (console_trylock()) {
 		console_unlock();
-		return NOTIFY_NONE;
+		return;
 	}
 
 	mdelay(50);
@@ -333,17 +329,18 @@ static int tegra_pm_flush_console(struct notifier_block *this,
 	else
 		pr_emerg("%s: Console was locked!\n", __func__);
 	console_unlock();
-	return NOTIFY_NONE;
 }
 
-static struct notifier_block tegra_reboot_notifier = {
-	.notifier_call = tegra_pm_flush_console,
-};
+static void tegra_pm_restart(char mode, const char *cmd)
+{
+	tegra_pm_flush_console();
+	arm_machine_restart(mode, cmd);
+}
 
 #ifdef CONFIG_ARCH_TEGRA_2x_SOC
 void __init tegra20_init_early(void)
 {
-	register_reboot_notifier(&tegra_reboot_notifier);
+	arm_pm_restart = tegra_pm_restart;
 #ifndef CONFIG_SMP
 	/* For SMP system, initializing the reset handler here is too
 	   late. For non-SMP systems, the function that calls the reset
