a6d8275dd91f41c3a530c57dedeac3982a3769ac
Haley Teng <hteng@nvidia.com>
2011-09-27 12:20:32 +0800

    arm: tegra: move ram console to common codes
    cleanup ram console related source codes and move most of the
    implementation in board files to common.c since ram console is a common
    debug mechanism.
    
    bug 873307
    
    Signed-off-by: Haley Teng <hteng@nvidia.com>
    Reviewed-on: http://git-master/r/54598
    Reviewed-on: http://git-master/r/66588
    (cherry picked from commit 119ce36b7bed370a528dfebc80bd79698118248d)
    
    Change-Id: I8b769b422305101a97f1fbc99db4af48dc7d4f25
    Reviewed-on: http://git-master/r/71961
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>
    Reviewed-on: http://git-master/r/74552
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    
    Rebase-Id: R39bcfa4d8bf9970631e00e824396c10c49a20aba

--- arch/arm/mach-tegra/board-cardhu.c
+++ arch/arm/mach-tegra/board-cardhu.c
@@ -701,19 +701,6 @@ static struct platform_device cardhu_audio_aic326x_device = {
 	},
 };
 
-static struct resource ram_console_resources[] = {
-	{
-		.flags = IORESOURCE_MEM,
-	},
-};
-
-static struct platform_device ram_console_device = {
-	.name 		= "ram_console",
-	.id 		= -1,
-	.num_resources	= ARRAY_SIZE(ram_console_resources),
-	.resource	= ram_console_resources,
-};
-
 static struct platform_device *cardhu_devices[] __initdata = {
 	&tegra_pmu_device,
 	&tegra_rtc_device,
@@ -749,7 +736,6 @@ static struct platform_device *cardhu_devices[] __initdata = {
 #if defined(CONFIG_CRYPTO_DEV_TEGRA_AES)
 	&tegra_aes_device,
 #endif
-	&ram_console_device,
 };
 
 #define MXT_CONFIG_CRC  0xD62DE8
@@ -1141,6 +1127,7 @@ static void __init tegra_cardhu_init(void)
 #endif
 	cardhu_uart_init();
 	platform_add_devices(cardhu_devices, ARRAY_SIZE(cardhu_devices));
+	tegra_ram_console_debug_init();
 	cardhu_sdhci_init();
 	cardhu_regulator_init();
 	cardhu_dtv_init();
@@ -1172,19 +1159,7 @@ static void __init cardhu_ramconsole_reserve(unsigned long size)
 	struct resource *res;
 	long ret;
 
-	res = platform_get_resource(&ram_console_device, IORESOURCE_MEM, 0);
-	if (!res) {
-		pr_err("Failed to find memory resource for ram console\n");
-		return;
-	}
-	res->start = memblock_end_of_DRAM() - size;
-	res->end = res->start + size - 1;
-	ret = memblock_remove(res->start, size);
-	if (ret) {
-		ram_console_device.resource = NULL;
-		ram_console_device.num_resources = 0;
-		pr_err("Failed to reserve memory block for ram console\n");
-	}
+	tegra_ram_console_debug_reserve(SZ_1M);
 }
 
 static void __init tegra_cardhu_reserve(void)
