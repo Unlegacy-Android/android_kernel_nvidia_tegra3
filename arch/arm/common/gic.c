d52d0ee88b417e4c37cb407305323bac8ad9f4be
Alex Frid <afrid@nvidia.com>
2011-10-11 22:59:38 -0700

    ARM: gic: Use affinity hint to set multiple CPUs for IRQ
    GIC IRQ affinity is currently set to one CPU only - the 1st cpu in
    the requested mask. This commit adds option to set IRQ affinity to
    all cpus present in affinity_hint and requested masks. The option is
    enabled by default on Tegra architecture starting with Tegra3.
    
    (cherry picked from commit 09f7ef4f28a6e18188649c40848252bc18a6646c)
    
    Change-Id: I0d655f1d39170382f3372294172ed6d02dc0ad49
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/69328
    Reviewed-by: Lokesh Pathak <lpathak@nvidia.com>
    Tested-by: Lokesh Pathak <lpathak@nvidia.com>
    
    Rebase-Id: R31f7a5ed91995a83d55d051a5926dc47f33b87ea

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -240,6 +240,9 @@ static int gic_set_affinity(struct irq_data *d, const struct cpumask *mask_val,
 	unsigned int shift = (gic_irq(d) % 4) * 8;
 	unsigned int cpu = cpumask_any_and(mask_val, cpu_online_mask);
 	u32 val, mask, bit;
+#ifdef CONFIG_GIC_SET_MULTIPLE_CPUS
+	struct irq_desc *desc = irq_to_desc(d->irq);
+#endif
 
 	if (cpu >= 8 || cpu >= nr_cpu_ids)
 		return -EINVAL;
@@ -249,7 +252,15 @@ static int gic_set_affinity(struct irq_data *d, const struct cpumask *mask_val,
 
 	raw_spin_lock(&irq_controller_lock);
 	val = readl_relaxed(reg) & ~mask;
-	writel_relaxed(val | bit, reg);
+	val |= bit;
+#ifdef CONFIG_GIC_SET_MULTIPLE_CPUS
+	if (desc && desc->affinity_hint) {
+		struct cpumask mask_hint;
+		if (cpumask_and(&mask_hint, desc->affinity_hint, mask_val))
+			val |= (*cpumask_bits(&mask_hint) << shift) & mask;
+	}
+#endif
+	writel_relaxed(val, reg);
 	raw_spin_unlock(&irq_controller_lock);
 
 	return IRQ_SET_MASK_OK;
