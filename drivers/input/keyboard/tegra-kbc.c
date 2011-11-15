a86bd837a40966f12dd02e6b066da5e425f4626c
Ray Poudrier <rapoudrier@nvidia.com>
2011-11-14 16:26:27 -0800

    input: tegra-kbc: add is_open, don't suspend if no clock
    Added is_open to know whether the clock has been disabled.
    Occasionally _close is called before _suspend during LP0 cycles
    resulting in a hang as the clock is disabled.
    
    Bug 855753
    
    Change-Id: Ida938ada7d14085e11096b25cf41431fc2d46215
    Reviewed-on: http://git-master/r/50218
    Reviewed-on: http://git-master/r/64257
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>
    
    Rebase-Id: Rdf07af7eda0f4c1c3ad5fe5733c5de3926545e31

--- drivers/input/keyboard/tegra-kbc.c
+++ drivers/input/keyboard/tegra-kbc.c
@@ -88,6 +88,7 @@ struct tegra_kbc {
 	u32 wakeup_key;
 	struct timer_list timer;
 	struct clk *clk;
+	int is_open;
 	unsigned long scan_timeout_count;
 	unsigned long one_scan_time;
 };
@@ -491,6 +492,7 @@ static int tegra_kbc_start(struct tegra_kbc *kbc)
 	u32 val = 0;
 
 	clk_enable(kbc->clk);
+	kbc->is_open = 1;
 
 	/* Reset the KBC controller to clear all previous status.*/
 	tegra_periph_reset_assert(kbc->clk);
@@ -558,6 +560,7 @@ static void tegra_kbc_stop(struct tegra_kbc *kbc)
 	del_timer_sync(&kbc->timer);
 
 	clk_disable(kbc->clk);
+	kbc->is_open = 0;
 }
 
 static int tegra_kbc_open(struct input_dev *dev)
@@ -732,6 +735,7 @@ static int __devinit tegra_kbc_probe(struct platform_device *pdev)
 		goto err_iounmap;
 	}
 
+	kbc->is_open = 0;
 	kbc->wake_enable_rows = 0;
 	kbc->wake_enable_cols = 0;
 	for (i = 0; i < pdata->wake_cnt; i++) {
@@ -858,6 +862,9 @@ static int tegra_kbc_suspend(struct device *dev)
 	int timeout;
 	unsigned long int_st;
 
+	if (!kbc->is_open)
+		return 0;
+
 	mutex_lock(&kbc->idev->mutex);
 	if (device_may_wakeup(&pdev->dev)) {
 		disable_irq(kbc->irq);
@@ -893,6 +900,9 @@ static int tegra_kbc_resume(struct device *dev)
 	struct platform_device *pdev = to_platform_device(dev);
 	struct tegra_kbc *kbc = platform_get_drvdata(pdev);
 	int err = 0;
+
+	if (!kbc->is_open)
+		return tegra_kbc_start(kbc);
 
 	mutex_lock(&kbc->idev->mutex);
 	if (device_may_wakeup(&pdev->dev)) {
