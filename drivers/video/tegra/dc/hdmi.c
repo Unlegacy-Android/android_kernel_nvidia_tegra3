0ff9b067d9152b501af662f82464e9021bdcf408
Jon Mayo <jmayo@nvidia.com>
2011-11-21 13:14:31 -0800

    video: tegra: dc: use Kconfig to enable nvhdcp
    Use TEGRA_NVHDCP to enable/disable support for NVHDCP on HDMI.
    
    Change-Id: If65267e7a4c82c5497271c19ac985bfa8881bca1
    Signed-off-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-on: http://git-master/r/67883
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    
    Rebase-Id: R0b199e66a9ccc7480b9773bf7cc3a045772cda64

--- drivers/video/tegra/dc/hdmi.c
+++ drivers/video/tegra/dc/hdmi.c
@@ -992,6 +992,7 @@ static int tegra_dc_hdmi_init(struct tegra_dc *dc)
 		goto err_free_irq;
 	}
 
+#ifdef CONFIG_TEGRA_NVHDCP
 	hdmi->nvhdcp = tegra_nvhdcp_create(hdmi, dc->ndev->id,
 			dc->out->dcc_bus);
 	if (IS_ERR_OR_NULL(hdmi->nvhdcp)) {
@@ -999,6 +1000,9 @@ static int tegra_dc_hdmi_init(struct tegra_dc *dc)
 		err = PTR_ERR(hdmi->nvhdcp);
 		goto err_edid_destroy;
 	}
+#else
+	hdmi->nvhdcp = NULL;
+#endif
 
 	INIT_DELAYED_WORK(&hdmi->work, tegra_dc_hdmi_detect_worker);
 
