d95dd7ec3cae4849324cfe8c6c3a99d5ebde3eac
Robert Collins <rcollins@nvidia.com>
2011-12-01 16:19:20 -0800

    mpu6050: Motion Libraries: Board files for Invensense MPU6050.
    Invensense board files for MPU6050. MPL version 4.1.1.
    
    Bug 825602
    
    Signed-off-by: Robert Collins <rcollins@nvidia.com>
    Reviewed-on: http://git-master/r/70373
    (cherry picked from commit 2839895906a9c822e5c9f8abab68e33e9982c5a9)
    
    Change-Id: I97eb4dcc075ab8b6f2bb583756dd10128924f10a
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-on: http://git-master/r/82717
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    Tested-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-by: Robert Collins <rcollins@nvidia.com>
    
    Rebase-Id: R2309f6c496cd642457025b133e862ea902e5bbcb

--- arch/arm/mach-tegra/board-enterprise.h
+++ arch/arm/mach-tegra/board-enterprise.h
@@ -53,7 +53,9 @@ int enterprise_edp_init(void);
 void enterprise_bpc_mgmt_init(void);
 
 /* Invensense MPU Definitions */
-#define MPU_GYRO_NAME		"mpu3050"
+#define MPU_TYPE_MPU3050	1
+#define MPU_TYPE_MPU6050	2
+#define MPU_GYRO_TYPE		MPU_TYPE_MPU3050
 #define MPU_GYRO_IRQ_GPIO	TEGRA_GPIO_PH4
 #define MPU_GYRO_ADDR		0x68
 #define MPU_GYRO_BUS_NUM	0
