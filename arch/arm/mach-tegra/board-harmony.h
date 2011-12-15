afca07a691db593768c11342ec58b97e02aa28b2
Bill Huang <bilhuang@nvidia.com>
2011-12-15 02:53:20 -0800

    arm: tegra: harmony: enable power key to wake up the system
    enable power button for waking up harmony
    
    bug 911107
    
    Change-Id: Id69204a904cafa4186cbac201830ac6d1e5a81b1
    Signed-off-by: Bill Huang <bilhuang@nvidia.com>
    Reviewed-on: http://git-master/r/70274
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>
    
    Rebase-Id: Rf4865d89bf0a2ee24fb598512dfa63ee8737a96d

--- arch/arm/mach-tegra/board-harmony.h
+++ arch/arm/mach-tegra/board-harmony.h
@@ -35,6 +35,8 @@
 #define TEGRA_GPIO_EXT_MIC_EN		TEGRA_GPIO_PX1
 #define TEGRA_GPIO_EN_VDD_1V05_GPIO	HARMONY_GPIO_TPS6586X(2)
 
+#define TEGRA_GPIO_POWERKEY		TEGRA_GPIO_PV2
+
 void harmony_pinmux_init(void);
 int harmony_regulator_init(void);
 int harmony_panel_init(void);
