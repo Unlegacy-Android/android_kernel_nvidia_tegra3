2325d13554c6e05dc5c04bde4ee9302580af8f23
Pritesh Raithatha <praithatha@nvidia.com>
2011-12-28 19:21:44 +0530

    power: bq27x00: add charging status update delay
    bq27x00 takes 3 to 4 second to update charging/discharging
    status so it requires to schedule work after 4 second on
    external powersupply change.
    
    Bug 902678
    
    Change-Id: Ic5b42804ee3cd98ffab762c042bad447934eba85
    Signed-off-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-on: http://git-master/r/72411
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    
    Rebase-Id: Rb904561b17e059f3dd47ed940bc38476fe363597

--- drivers/power/bq27x00_battery.c
+++ drivers/power/bq27x00_battery.c
@@ -82,6 +82,8 @@
 #define BQ27510_CNTL_SET_SLEEP		0x0013
 #define BQ27510_CNTL_CLEAR_SLEEP	0x0014
 
+/* bq27x00 requires 3 to 4 second to update charging status */
+#define CHARGING_STATUS_UPDATE_DELAY_SECS	4
 
 struct bq27x00_device_info;
 struct bq27x00_access_methods {
@@ -116,12 +118,14 @@ struct bq27x00_device_info {
 
 	unsigned long last_update;
 	struct delayed_work work;
+	struct delayed_work external_power_changed_work;
 
 	struct power_supply	bat;
 
 	struct bq27x00_access_methods bus;
 
 	struct mutex lock;
+	struct mutex update_lock;
 };
 
 static enum power_supply_property bq27x00_battery_props[] = {
@@ -369,6 +373,7 @@ static void bq27x00_update(struct bq27x00_device_info *di)
 	struct bq27x00_reg_cache cache = {0, };
 	bool is_bq27500 = (di->chip == BQ27500 || di->chip == BQ27510);
 
+	mutex_lock(&di->update_lock);
 	cache.flags = bq27x00_read(di, BQ27x00_REG_FLAGS, !is_bq27500);
 	if (cache.flags >= 0) {
 		if (!is_bq27500 && (cache.flags & BQ27000_FLAG_CI)) {
@@ -401,6 +406,7 @@ static void bq27x00_update(struct bq27x00_device_info *di)
 	}
 
 	di->last_update = jiffies;
+	mutex_unlock(&di->update_lock);
 }
 
 static void bq27x00_battery_poll(struct work_struct *work)
@@ -415,6 +421,15 @@ static void bq27x00_battery_poll(struct work_struct *work)
 		set_timer_slack(&di->work.timer, poll_interval * HZ / 4);
 		schedule_delayed_work(&di->work, poll_interval * HZ);
 	}
+}
+
+static void bq27x00_external_power_changed_work(struct work_struct *work)
+{
+	struct bq27x00_device_info *di =
+		container_of(work, struct bq27x00_device_info,
+				external_power_changed_work.work);
+
+	bq27x00_update(di);
 }
 
 /*
@@ -677,12 +692,19 @@ static int bq27x00_battery_get_property(struct power_supply *psy,
 	return ret;
 }
 
+static unsigned int charging_update_delay_secs =
+				CHARGING_STATUS_UPDATE_DELAY_SECS;
+module_param(charging_update_delay_secs, uint, 0644);
+MODULE_PARM_DESC(charging_update_delay_secs, "battery charging " \
+			"status update delay in seconds");
+
 static void bq27x00_external_power_changed(struct power_supply *psy)
 {
 	struct bq27x00_device_info *di = to_bq27x00_device_info(psy);
 
-	cancel_delayed_work_sync(&di->work);
-	schedule_delayed_work(&di->work, 0);
+	cancel_delayed_work_sync(&di->external_power_changed_work);
+	schedule_delayed_work(&di->external_power_changed_work,
+		charging_update_delay_secs * HZ);
 }
 
 static int bq27x00_powersupply_init(struct bq27x00_device_info *di)
@@ -696,7 +718,10 @@ static int bq27x00_powersupply_init(struct bq27x00_device_info *di)
 	di->bat.external_power_changed = bq27x00_external_power_changed;
 
 	INIT_DELAYED_WORK(&di->work, bq27x00_battery_poll);
+	INIT_DELAYED_WORK(&di->external_power_changed_work,
+				bq27x00_external_power_changed_work);
 	mutex_init(&di->lock);
+	mutex_init(&di->update_lock);
 
 	ret = power_supply_register(di->dev, &di->bat);
 	if (ret) {
@@ -722,10 +747,11 @@ static void bq27x00_powersupply_unregister(struct bq27x00_device_info *di)
 	poll_interval = 0;
 
 	cancel_delayed_work_sync(&di->work);
-
+	cancel_delayed_work_sync(&di->external_power_changed_work);
 	power_supply_unregister(&di->bat);
 
 	mutex_destroy(&di->lock);
+	mutex_destroy(&di->update_lock);
 }
 
 
