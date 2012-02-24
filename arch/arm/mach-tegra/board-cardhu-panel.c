f39e5c4aa5b24a70786cdd4a9069892412b815c3
Mayuresh Kulkarni <mkulkarni@nvidia.com>
2012-02-24 15:46:51 +0530

    video: tegra: host: Merge tegra_grhost and host1x devices
    - tegra_grhost is a platform device that represents host1x
    - nvhost has device host1x which represents the same hardware
    - merge these two device structs
    - as the new struct is a nvhost_device, platform_driver
    is also converted into a nvhost_driver
    - register nvhost device before other graphics devices.
    this ensures that nvhost_probe() is called as soon as
    nvhost_driver is registered with the core.
    - this also ensures that nvmap is probed first, followed
    by nvhost, followed by tegra-dc and nvavp (if they
    are enabled).
    
    Change-Id: Ic420a6516a9cb20d6f481692a4db10fa6053dd90
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    Reviewed-on: http://git-master/r/82631
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>
    
    Rebase-Id: R9752f6e1b8641de72a5f833c6ae59108c368476f

--- arch/arm/mach-tegra/board-cardhu-panel.c
+++ arch/arm/mach-tegra/board-cardhu-panel.c
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-tegra/board-cardhu-panel.c
  *
- * Copyright (c) 2010-2011, NVIDIA Corporation.
+ * Copyright (c) 2010-2012, NVIDIA Corporation.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -1035,9 +1035,6 @@ static struct platform_device cardhu_nvmap_device = {
 static struct platform_device *cardhu_gfx_devices[] __initdata = {
 #if defined(CONFIG_TEGRA_NVMAP)
 	&cardhu_nvmap_device,
-#endif
-#ifdef CONFIG_TEGRA_GRHOST
-	&tegra_grhost_device,
 #endif
 	&tegra_pwfm0_device,
 	&cardhu_backlight_device,
