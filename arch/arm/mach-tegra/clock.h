8591e556adf71b2955507a8c4affa32a9ef75d7a
Alex Frid <afrid@nvidia.com>
2012-03-17 23:38:07 -0700

    ARM: tegra: clock: Add locked version of round rate
    Add locked version of round rate API to be used by tegra arch
    specific layer.
    
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    (cherry picked from commit 457627966b91f2141439812869adc4acf9242471)
    
    Change-Id: Id68d0bb952d1e7d9e650341872d1b06b0b2d3cea
    Reviewed-on: http://git-master/r/100474
    Reviewed-by: Automatic_Commit_Validation_User
    Tested-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- arch/arm/mach-tegra/clock.h
+++ arch/arm/mach-tegra/clock.h
@@ -242,6 +242,7 @@ unsigned long clk_get_min_rate(struct clk *c);
 unsigned long clk_get_rate_locked(struct clk *c);
 int clk_set_rate_locked(struct clk *c, unsigned long rate);
 int clk_set_parent_locked(struct clk *c, struct clk *parent);
+long clk_round_rate_locked(struct clk *c, unsigned long rate);
 int tegra_clk_shared_bus_update(struct clk *c);
 void tegra3_set_cpu_skipper_delay(int delay);
 int tegra_emc_set_rate(unsigned long rate);
