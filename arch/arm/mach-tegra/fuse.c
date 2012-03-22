mmontuori/kernel-tegra-google

[ARM] tegra: add utility function to read spare fuse bits
spare fuse bits 18 and 19 are used to distinguish A03p Tegra 2
chips from A03 chips. this is needed on some platforms to
determine whether or not LP0 suspend should be enabled.

Change-Id: I899213515949c8f13b04a33f75ecfb9de99b4dcd
Signed-off-by: Gary King <gking@nvidia.com>

arch/arm/mach-tegra/fuse.c
@@ -59,6 +59,12 @@ unsigned long long tegra_chip_uid(void)
 	return (hi << 32ull) | lo;
 }
 
+unsigned int tegra_spare_fuse(int bit)
+{
+	BUG_ON(bit < 0 || bit > 61);
+	return fuse_readl(FUSE_SPARE_BIT + bit * 4);
+}
+
 int tegra_sku_id(void)
 {
 	int sku_id;

Unlegacy-Android/android_kernel_nvidia_tegra3

Original-Change-Id: I03a964eac3783535357faecee8cd35e65350b356
Reviewed-on: http://git-master/r/12078
Tested-by: Sachin Nikam <snikam@nvidia.com>
Reviewed-by: Narendra Damahe <ndamahe@nvidia.com>
Tested-by: Narendra Damahe <ndamahe@nvidia.com>
Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
Rebase-Id: Rd59fb4d4002b05d88529c44bb7a8d5c5b6bff79b

arch/arm/mach-tegra/fuse.c
@@ -73,6 +73,12 @@ unsigned long long tegra_chip_uid(void)
 	return (hi << 32ull) | lo;
 }
 
+unsigned int tegra_spare_fuse(int bit)
+{
+	BUG_ON(bit < 0 || bit > 61);
+	return tegra_fuse_readl(FUSE_SPARE_BIT + bit * 4);
+}
+
 int tegra_sku_id(void)
 {
 	int sku_id;