CyanogenMod/lge-kernel-star

ARM: tegra: Rearrange pm.c for cleaner ifdefs
Change-Id: I02a8a10ee842bcc02d590f116484f4ff8d0705b0
Signed-off-by: Scott Williams <scwilliams@nvidia.com>
DW: Split into logical changes
Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

arch/arm/mach-tegra/pm.c
@@ -205,34 +205,6 @@ static unsigned long
 #define tegra_cluster_switch_time(flags, id) do {} while(0)
 #endif
 
-#ifdef CONFIG_PM_SLEEP
-static void tegra_suspend_check_pwr_stats(void)
-{
-	/* cpus and l2 are powered off later */
-	unsigned long pwrgate_partid_mask =
-#if !defined(CONFIG_ARCH_TEGRA_2x_SOC)
-		(1 << TEGRA_POWERGATE_HEG)	|
-		(1 << TEGRA_POWERGATE_SATA)	|
-		(1 << TEGRA_POWERGATE_3D1)	|
-#endif
-		(1 << TEGRA_POWERGATE_3D)	|
-		(1 << TEGRA_POWERGATE_VENC)	|
-		(1 << TEGRA_POWERGATE_PCIE)	|
-		(1 << TEGRA_POWERGATE_VDEC)	|
-		(1 << TEGRA_POWERGATE_MPE);
-
-	int partid;
-
-	for (partid = 0; partid < TEGRA_NUM_POWERGATE; partid++)
-		if ((1 << partid) & pwrgate_partid_mask)
-			if (tegra_powergate_is_powered(partid))
-				pr_warning("partition %s is left on before suspend\n",
-					tegra_powergate_get_name(partid));
-
-	return;
-}
-#endif
-
 unsigned long tegra_cpu_power_good_time(void)
 {
 	if (WARN_ON_ONCE(!pdata))
@@ -255,6 +227,27 @@ unsigned long tegra_cpu_lp2_min_residency(void)
 		return 2000;
 
 	return pdata->cpu_lp2_min_residency;
+}
+
+/*
+ * create_suspend_pgtable
+ *
+ * Creates a page table with identity mappings of physical memory and IRAM
+ * for use when the MMU is off, in addition to all the regular kernel mappings.
+ */
+static int create_suspend_pgtable(void)
+{
+	tegra_pgd = pgd_alloc(&init_mm);
+	if (!tegra_pgd)
+		return -ENOMEM;
+
+	identity_mapping_add(tegra_pgd, PLAT_PHYS_OFFSET, IO_IRAM_PHYS);
+	identity_mapping_add(tegra_pgd, IO_IRAM_PHYS,
+		IO_IRAM_PHYS + SECTION_SIZE);
+
+	tegra_pgd_phys = virt_to_phys(tegra_pgd);
+
+	return 0;
 }
 
 #ifdef CONFIG_PM_SLEEP
@@ -289,30 +282,7 @@ static void set_power_timers(unsigned long us_on, unsigned long us_off,
 	}
 	tegra_last_pclk = pclk;
 }
-#endif
 
-/*
- * create_suspend_pgtable
- *
- * Creates a page table with identity mappings of physical memory and IRAM
- * for use when the MMU is off, in addition to all the regular kernel mappings.
- */
-static int create_suspend_pgtable(void)
-{
-	tegra_pgd = pgd_alloc(&init_mm);
-	if (!tegra_pgd)
-		return -ENOMEM;
-
-	identity_mapping_add(tegra_pgd, PLAT_PHYS_OFFSET, IO_IRAM_PHYS);
-	identity_mapping_add(tegra_pgd, IO_IRAM_PHYS,
-		IO_IRAM_PHYS + SECTION_SIZE);
-
-	tegra_pgd_phys = virt_to_phys(tegra_pgd);
-
-	return 0;
-}
-
-#ifdef CONFIG_PM_SLEEP
 /*
  * restore_cpu_complex
  *
@@ -642,6 +612,32 @@ static const char *lp_state[TEGRA_MAX_SUSPEND_MODE] = {
 static int tegra_suspend_enter(suspend_state_t state)
 {
 	return tegra_suspend_dram(current_suspend_mode);
+}
+
+static void tegra_suspend_check_pwr_stats(void)
+{
+	/* cpus and l2 are powered off later */
+	unsigned long pwrgate_partid_mask =
+#if !defined(CONFIG_ARCH_TEGRA_2x_SOC)
+		(1 << TEGRA_POWERGATE_HEG)	|
+		(1 << TEGRA_POWERGATE_SATA)	|
+		(1 << TEGRA_POWERGATE_3D1)	|
+#endif
+		(1 << TEGRA_POWERGATE_3D)	|
+		(1 << TEGRA_POWERGATE_VENC)	|
+		(1 << TEGRA_POWERGATE_PCIE)	|
+		(1 << TEGRA_POWERGATE_VDEC)	|
+		(1 << TEGRA_POWERGATE_MPE);
+
+	int partid;
+
+	for (partid = 0; partid < TEGRA_NUM_POWERGATE; partid++)
+		if ((1 << partid) & pwrgate_partid_mask)
+			if (tegra_powergate_is_powered(partid))
+				pr_warning("partition %s is left on before suspend\n",
+					tegra_powergate_get_name(partid));
+
+	return;
 }
 
 int tegra_suspend_dram(enum tegra_suspend_mode mode)

Unlegacy-Android/android_kernel_nvidia_tegra3

Rebase-Id: R1105e3976f266bf4e229510c8c5ae876f900b2ce

arch/arm/mach-tegra/pm.c
@@ -206,34 +206,6 @@ static unsigned long
 #define tegra_cluster_switch_time(flags, id) do {} while(0)
 #endif
 
-#ifdef CONFIG_PM_SLEEP
-static void tegra_suspend_check_pwr_stats(void)
-{
-	/* cpus and l2 are powered off later */
-	unsigned long pwrgate_partid_mask =
-#if !defined(CONFIG_ARCH_TEGRA_2x_SOC)
-		(1 << TEGRA_POWERGATE_HEG)	|
-		(1 << TEGRA_POWERGATE_SATA)	|
-		(1 << TEGRA_POWERGATE_3D1)	|
-#endif
-		(1 << TEGRA_POWERGATE_3D)	|
-		(1 << TEGRA_POWERGATE_VENC)	|
-		(1 << TEGRA_POWERGATE_PCIE)	|
-		(1 << TEGRA_POWERGATE_VDEC)	|
-		(1 << TEGRA_POWERGATE_MPE);
-
-	int partid;
-
-	for (partid = 0; partid < TEGRA_NUM_POWERGATE; partid++)
-		if ((1 << partid) & pwrgate_partid_mask)
-			if (tegra_powergate_is_powered(partid))
-				pr_warning("partition %s is left on before suspend\n",
-					tegra_powergate_get_name(partid));
-
-	return;
-}
-#endif
-
 unsigned long tegra_cpu_power_good_time(void)
 {
 	if (WARN_ON_ONCE(!pdata))
@@ -256,6 +228,27 @@ unsigned long tegra_cpu_lp2_min_residency(void)
 		return 2000;
 
 	return pdata->cpu_lp2_min_residency;
+}
+
+/*
+ * create_suspend_pgtable
+ *
+ * Creates a page table with identity mappings of physical memory and IRAM
+ * for use when the MMU is off, in addition to all the regular kernel mappings.
+ */
+static int create_suspend_pgtable(void)
+{
+	tegra_pgd = pgd_alloc(&init_mm);
+	if (!tegra_pgd)
+		return -ENOMEM;
+
+	identity_mapping_add(tegra_pgd, PHYS_OFFSET, IO_IRAM_PHYS);
+	identity_mapping_add(tegra_pgd, IO_IRAM_PHYS,
+		IO_IRAM_PHYS + SECTION_SIZE);
+
+	tegra_pgd_phys = virt_to_phys(tegra_pgd);
+
+	return 0;
 }
 
 #ifdef CONFIG_PM_SLEEP
@@ -290,30 +283,7 @@ static void set_power_timers(unsigned long us_on, unsigned long us_off,
 	}
 	tegra_last_pclk = pclk;
 }
-#endif
 
-/*
- * create_suspend_pgtable
- *
- * Creates a page table with identity mappings of physical memory and IRAM
- * for use when the MMU is off, in addition to all the regular kernel mappings.
- */
-static int create_suspend_pgtable(void)
-{
-	tegra_pgd = pgd_alloc(&init_mm);
-	if (!tegra_pgd)
-		return -ENOMEM;
-
-	identity_mapping_add(tegra_pgd, PHYS_OFFSET, IO_IRAM_PHYS);
-	identity_mapping_add(tegra_pgd, IO_IRAM_PHYS,
-		IO_IRAM_PHYS + SECTION_SIZE);
-
-	tegra_pgd_phys = virt_to_phys(tegra_pgd);
-
-	return 0;
-}
-
-#ifdef CONFIG_PM_SLEEP
 /*
  * restore_cpu_complex
  *
@@ -643,6 +613,32 @@ static const char *lp_state[TEGRA_MAX_SUSPEND_MODE] = {
 static int tegra_suspend_enter(suspend_state_t state)
 {
 	return tegra_suspend_dram(current_suspend_mode);
+}
+
+static void tegra_suspend_check_pwr_stats(void)
+{
+	/* cpus and l2 are powered off later */
+	unsigned long pwrgate_partid_mask =
+#if !defined(CONFIG_ARCH_TEGRA_2x_SOC)
+		(1 << TEGRA_POWERGATE_HEG)	|
+		(1 << TEGRA_POWERGATE_SATA)	|
+		(1 << TEGRA_POWERGATE_3D1)	|
+#endif
+		(1 << TEGRA_POWERGATE_3D)	|
+		(1 << TEGRA_POWERGATE_VENC)	|
+		(1 << TEGRA_POWERGATE_PCIE)	|
+		(1 << TEGRA_POWERGATE_VDEC)	|
+		(1 << TEGRA_POWERGATE_MPE);
+
+	int partid;
+
+	for (partid = 0; partid < TEGRA_NUM_POWERGATE; partid++)
+		if ((1 << partid) & pwrgate_partid_mask)
+			if (tegra_powergate_is_powered(partid))
+				pr_warning("partition %s is left on before suspend\n",
+					tegra_powergate_get_name(partid));
+
+	return;
 }
 
 int tegra_suspend_dram(enum tegra_suspend_mode mode)