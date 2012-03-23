CyanogenMod/lge-kernel-star

ARM: tegra: power: Fix suspend pgd identity mapping
The RAM identity mapping in the suspend pgd was based upon the
characteristics of the Tegra2 address map and would not work for
Tegra3. Change the mapping so that it's independent of the physical
address map characteristics.

Change-Id: Ib8f67c169f6b0988e88a4ef7616dfd48e66754ac
Signed-off-by: Scott Williams <scwilliams@nvidia.com>

arch/arm/mach-tegra/pm.c
@@ -39,6 +39,7 @@
 #include <linux/uaccess.h>
 #include <linux/syscore_ops.h>
 #include <linux/vmalloc.h>
+#include <linux/memblock.h>
 
 #include <asm/cacheflush.h>
 #include <asm/cpu_pm.h>
@@ -237,7 +238,8 @@ static __init int create_suspend_pgtable(void)
 	if (!tegra_pgd)
 		return -ENOMEM;
 
-	identity_mapping_add(tegra_pgd, PLAT_PHYS_OFFSET, IO_IRAM_PHYS);
+	identity_mapping_add(tegra_pgd, PLAT_PHYS_OFFSET,
+			     PLAT_PHYS_OFFSET + memblock_phys_mem_size());
 	identity_mapping_add(tegra_pgd, IO_IRAM_PHYS,
 		IO_IRAM_PHYS + SECTION_SIZE);
 

Unlegacy-Android/android_kernel_nvidia_tegra3

Rebase-Id: R399b9624e8f25637538273642059e8d0719badf5

arch/arm/mach-tegra/pm.c
@@ -40,6 +40,7 @@
 #include <linux/syscore_ops.h>
 #include <linux/cpu_pm.h>
 #include <linux/vmalloc.h>
+#include <linux/memblock.h>
 
 #include <asm/cacheflush.h>
 #include <asm/hardware/cache-l2x0.h>
@@ -238,7 +239,8 @@ static __init int create_suspend_pgtable(void)
 	if (!tegra_pgd)
 		return -ENOMEM;
 
-	identity_mapping_add(tegra_pgd, PHYS_OFFSET, IO_IRAM_PHYS);
+	identity_mapping_add(tegra_pgd, PHYS_OFFSET,
+			     PHYS_OFFSET + memblock_phys_mem_size());
 	identity_mapping_add(tegra_pgd, IO_IRAM_PHYS,
 		IO_IRAM_PHYS + SECTION_SIZE);
 