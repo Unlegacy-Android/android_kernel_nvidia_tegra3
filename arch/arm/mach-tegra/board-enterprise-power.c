fosser2/android-tegra-nv-3.1.10-rel-15r7

ARM: tegra: cardhu: switch off PMU at high temperature
Add board support needed for PMU switch off when tsensor
detects temperature > TH3 threshold set.

bug 850047

Change-Id: I7a283cedc735264dd8ea52801f7f1a103e9293cb
Reviewed-on: http://git-master/r/41531
Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
Tested-by: Varun Colbert <vcolbert@nvidia.com>

arch/arm/mach-tegra/board-enterprise-power.c
@@ -401,6 +401,11 @@ static void enterprise_power_off(void)
 	while(1);
 }
 
+void __init tegra_tsensor_init(void)
+{
+/* To be implemented */
+}
+
 int __init enterprise_regulator_init(void)
 {
 	void __iomem *pmc = IO_ADDRESS(TEGRA_PMC_BASE);

Unlegacy-Android/android_kernel_nvidia_tegra3

Original-Change-Id: I7a283cedc735264dd8ea52801f7f1a103e9293cb
Rebase-Id: R3598ee532939c7dc2de990373958be4762442ffa

arch/arm/mach-tegra/board-enterprise-power.c
@@ -400,6 +400,11 @@ static void enterprise_power_off(void)
 	while(1);
 }
 
+void __init enterprise_tsensor_init(void)
+{
+/* To be implemented */
+}
+
 int __init enterprise_regulator_init(void)
 {
 	void __iomem *pmc = IO_ADDRESS(TEGRA_PMC_BASE);