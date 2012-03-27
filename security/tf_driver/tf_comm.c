5651b44cb9aeefb6ad3eae79f8295fad4ee87ede
Dan Willemsen <dwillemsen@nvidia.com>
2012-03-26 21:16:19 -0700

    HACK: make TF compile
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>

--- security/tf_driver/tf_comm.c
+++ security/tf_driver/tf_comm.c
@@ -1250,19 +1250,11 @@ static int tf_send_recv(struct tf_comm *comm,
 	bool wait_prepared = false;
 	enum TF_COMMAND_STATE command_status = TF_COMMAND_STATE_PENDING;
 	DEFINE_WAIT(wait);
-#ifdef CONFIG_FREEZER
-	unsigned long saved_flags;
-#endif
 	dprintk(KERN_INFO "[pid=%d] tf_send_recv(%p)\n",
 		 current->pid, command);
 
 #ifdef CONFIG_TF_ZEBRA
 	tf_clock_timer_start();
-#endif
-
-#ifdef CONFIG_FREEZER
-	saved_flags = current->flags;
-	current->flags |= PF_FREEZER_NOSIG;
 #endif
 
 	/*
@@ -1283,7 +1275,7 @@ copy_answers:
 
 		dprintk(KERN_INFO
 			"Entering refrigerator.\n");
-		refrigerator();
+		try_to_freeze();
 		dprintk(KERN_INFO
 			"Left refrigerator.\n");
 		goto copy_answers;
@@ -1432,11 +1424,6 @@ exit:
 		finish_wait(&comm->wait_queue, &wait);
 		wait_prepared = false;
 	}
-
-#ifdef CONFIG_FREEZER
-	current->flags &= ~(PF_FREEZER_NOSIG);
-	current->flags |= (saved_flags & PF_FREEZER_NOSIG);
-#endif
 
 	return result;
 }
