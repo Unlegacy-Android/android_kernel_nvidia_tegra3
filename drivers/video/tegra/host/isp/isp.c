f6cb5c7789036fffa026690c975851886d06b678
Mayuresh Kulkarni <mkulkarni@nvidia.com>
2012-04-18 10:34:34 +0530

    video: tegra: host: move resources to devices
    - as of now the resources needed by all the host1x
    modules are part of resource list of host1x device
    - now that each module is a nvhost_device of its own,
    so it should hold the resources it needs instead of
    host1x device holding it for them
    - each device that needs its resources gets it in its
    _probe() using a helper API in bus_client.c
    
    Bug 871237
    
    Change-Id: Ia7c63fcf40cbc5db76d4d8339294a0e9ce75e352
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    Reviewed-on: http://git-master/r/94166
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>
    
    Conflicts:
    
    	drivers/video/tegra/host/isp/isp.c
    	drivers/video/tegra/host/mpe/mpe.c
    	drivers/video/tegra/host/vi/vi.c
    
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- drivers/video/tegra/host/isp/isp.c
+++ drivers/video/tegra/host/isp/isp.c
@@ -19,12 +19,21 @@
  */
 
 #include <linux/export.h>
+#include <linux/resource.h>
+
+#include <mach/iomap.h>
 
 #include "dev.h"
 #include "bus_client.h"
 
 static int __devinit isp_probe(struct nvhost_device *dev)
 {
+	int err = 0;
+
+	err = nvhost_client_device_get_resources(dev);
+	if (err)
+		return err;
+
 	return nvhost_client_device_init(dev);
 }
 
@@ -44,6 +53,13 @@ static int isp_resume(struct nvhost_device *dev)
 	dev_info(&dev->dev, "resuming\n");
 	return 0;
 }
+
+static struct resource isp_resources = {
+	.name = "regs",
+	.start = TEGRA_ISP_BASE,
+	.end = TEGRA_ISP_BASE + TEGRA_ISP_SIZE - 1,
+	.flags = IORESOURCE_MEM,
+};
 
 struct nvhost_device *isp_device;
 
@@ -68,6 +84,8 @@ static int __init isp_init(void)
 	if (!isp_device)
 		return -ENXIO;
 
+	isp_device->resource = &isp_resources;
+	isp_device->num_resources = 1;
 	err = nvhost_device_register(isp_device);
 	if (err)
 		return err;
