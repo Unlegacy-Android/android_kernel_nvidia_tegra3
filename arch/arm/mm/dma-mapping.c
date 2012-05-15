7a9a3f2626493d39518c02e327af6248136b89e6
Marek Szyprowski <m.szyprowski@samsung.com>
2012-05-15 19:04:13 +0200

    ARM: dma-mapping: use dma_mmap_from_coherent()
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    
    Conflicts:
    
    	arch/arm/mm/dma-mapping.c
    
    Change-Id: I69eae1abd7baa42a2c93e13c3efdba05ba26cdab
    Signed-off-by: Hiroshi DOYU <hdoyu@nvidia.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -639,6 +639,11 @@ static int dma_mmap(struct device *dev, struct vm_area_struct *vma,
 	int ret = -ENXIO;
 #ifdef CONFIG_MMU
 	unsigned long pfn = dma_to_pfn(dev, dma_addr);
+	vma->vm_page_prot = __get_dma_pgprot(attrs, vma->vm_page_prot);
+
+	if (dma_mmap_from_coherent(dev, vma, cpu_addr, size, &ret))
+		return ret;
+
 	ret = remap_pfn_range(vma, vma->vm_start,
 			      pfn + vma->vm_pgoff,
 			      vma->vm_end - vma->vm_start,
