2c8feae5d5c498a879997ce0296f33c47addcfb6
Varun Wadekar <vwadekar@nvidia.com>
2012-05-25 09:54:48 +0530

    Revert "Revert "Input: add infrastructure for selecting clockid for event time stamps""
    This reverts commit 0fbe99f35b72318edf753c13011ff9f7f27998c0.
    
    Conflicts:
    
    	drivers/input/evdev.c
    
    Change-Id: I0eb4362008afa882ed31d377d7f40ef3ca18a03e
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- kernel/time/timekeeping.c
+++ kernel/time/timekeeping.c
@@ -1261,6 +1261,8 @@ ktime_t ktime_get_monotonic_offset(void)
 
 	return timespec_to_ktime(wtom);
 }
+EXPORT_SYMBOL_GPL(ktime_get_monotonic_offset);
+
 
 /**
  * xtime_update() - advances the timekeeping infrastructure
