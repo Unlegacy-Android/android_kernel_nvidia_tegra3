89cf16f74f8448c9bfea406cb78f037bc85dde10
Alex Frid <afrid@nvidia.com>
2012-05-26 21:54:36 -0700

    ARM: tegra: dvfs: Alter CPU dvfs on EDP events
    Extended EDP processing of cpu up/down events with calls to alter CPU
    dvfs table. This is in addition to already supported changing of CPU
    dvfs on EDP thermal event. For now, added calls do not actually alter
    the table.
    
    Change-Id: I1cbf2c54eeca8dea1e7b6f4c65d8dbaf563a980e
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/104883
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- arch/arm/mach-tegra/cpu-tegra.c
+++ arch/arm/mach-tegra/cpu-tegra.c
@@ -241,12 +241,12 @@ int tegra_edp_update_thermal_zone(int temperature)
 
 	/* Update cpu rate if cpufreq (at least on cpu0) is already started;
 	   alter cpu dvfs table for this thermal zone if necessary */
-	tegra_cpu_dvfs_alter(edp_thermal_index, true);
+	tegra_cpu_dvfs_alter(edp_thermal_index, &edp_cpumask, true);
 	if (target_cpu_speed[0]) {
 		edp_update_limit();
 		tegra_cpu_set_speed_cap(NULL);
 	}
-	tegra_cpu_dvfs_alter(edp_thermal_index, false);
+	tegra_cpu_dvfs_alter(edp_thermal_index, &edp_cpumask, false);
 	mutex_unlock(&tegra_cpu_lock);
 
 	return ret;
@@ -320,6 +320,7 @@ static int tegra_cpu_edp_notify(
 	case CPU_UP_PREPARE:
 		mutex_lock(&tegra_cpu_lock);
 		cpu_set(cpu, edp_cpumask);
+		tegra_cpu_dvfs_alter(edp_thermal_index, &edp_cpumask, true);
 		edp_update_limit();
 
 		cpu_speed = tegra_getspeed(0);
@@ -334,13 +335,16 @@ static int tegra_cpu_edp_notify(
 			printk(KERN_DEBUG "tegra CPU:%sforce EDP limit %u kHz"
 				"\n", ret ? " failed to " : " ", new_speed);
 		}
+		tegra_cpu_dvfs_alter(edp_thermal_index, &edp_cpumask, false);
 		mutex_unlock(&tegra_cpu_lock);
 		break;
 	case CPU_DEAD:
 		mutex_lock(&tegra_cpu_lock);
 		cpu_clear(cpu, edp_cpumask);
+		tegra_cpu_dvfs_alter(edp_thermal_index, &edp_cpumask, true);
 		edp_update_limit();
 		tegra_cpu_set_speed_cap(NULL);
+		tegra_cpu_dvfs_alter(edp_thermal_index, &edp_cpumask, false);
 		mutex_unlock(&tegra_cpu_lock);
 		break;
 	}
