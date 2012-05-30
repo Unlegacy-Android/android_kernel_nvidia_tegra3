b175e51c671cf9418fdef87077bc876228320dbd
Dan Willemsen <dwillemsen@nvidia.com>
2012-05-30 00:36:39 -0700

    ARM: tegra: wakeups: Refactor
    TODO: Share code between T2 & T3.
    
    Move irq->wake translation to the driver layer, this lets us split out
    the tegra_irq_to_wake gpio searching to a tegra_gpio_to_wake function.
    
    tegra_pm_irq_set_wake: the tegra_*_to_wake() functions can never return
    -EALREADY or -ENOTSUPP (even before this change)
    
    This has a side effect of removing usage of TEGRA_GPIO_TO_IRQ, which is
    deprecated.
    
    Change-Id: I07c00952997db9f1597fd0a1caadbf4dfe1b5045
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>
    Reviewed-on: http://git-master/r/106365
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    Tested-by: Varun Wadekar <vwadekar@nvidia.com>

--- arch/arm/mach-tegra/wakeups-t2.c
+++ arch/arm/mach-tegra/wakeups-t2.c
@@ -20,27 +20,62 @@
 #include <mach/gpio-tegra.h>
 #include <mach/iomap.h>
 #include <mach/irqs.h>
-#include <mach/gpio.h>
 
 #include "gpio-names.h"
 
+/* TODO: We could populate the other table from this one at runtime
+ * instead of always searching twice */
+static int tegra_gpio_wakes[] = {
+	[0]  = TEGRA_GPIO_PO5,
+	[1]  = TEGRA_GPIO_PV3,
+	[2]  = TEGRA_GPIO_PL1,
+	[3]  = TEGRA_GPIO_PB6,
+	[4]  = TEGRA_GPIO_PN7,
+	[5]  = TEGRA_GPIO_PA0,
+	[6]  = TEGRA_GPIO_PU5,
+	[7]  = TEGRA_GPIO_PU6,
+	[8]  = TEGRA_GPIO_PC7,
+	[9]  = TEGRA_GPIO_PS2,
+	[10] = TEGRA_GPIO_PAA1,
+	[11] = TEGRA_GPIO_PW3,
+	[12] = TEGRA_GPIO_PW2,
+	[13] = TEGRA_GPIO_PY6,
+	[14] = TEGRA_GPIO_PV6,
+	[15] = TEGRA_GPIO_PJ7,
+	[16] = -EINVAL,
+	[17] = -EINVAL,
+	[18] = -EINVAL,
+	[19] = -EINVAL,	 /* TEGRA_USB1_VBUS, */
+	[20] = -EINVAL, /* TEGRA_USB3_VBUS, */
+	[21] = -EINVAL, /* TEGRA_USB1_ID, */
+	[22] = -EINVAL, /* TEGRA_USB3_ID, */
+	[23] = TEGRA_GPIO_PI5,
+	[24] = TEGRA_GPIO_PV2,
+	[25] = TEGRA_GPIO_PS4,
+	[26] = TEGRA_GPIO_PS5,
+	[27] = TEGRA_GPIO_PS0,
+	[28] = TEGRA_GPIO_PQ6,
+	[29] = TEGRA_GPIO_PQ7,
+	[30] = TEGRA_GPIO_PN2,
+};
+
 static int tegra_wake_event_irq[] = {
-	[0]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PO5),
-	[1]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PV3),
-	[2]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PL1),
-	[3]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PB6),
-	[4]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PN7),
-	[5]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PA0),
-	[6]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PU5),
-	[7]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PU6),
-	[8]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PC7),
-	[9]  = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PS2),
-	[10] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PAA1),
-	[11] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PW3),
-	[12] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PW2),
-	[13] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PY6),
-	[14] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PV6),
-	[15] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PJ7),
+	[0]  = -EAGAIN, /* Search the GPIO table */
+	[1]  = -EAGAIN,
+	[2]  = -EAGAIN,
+	[3]  = -EAGAIN,
+	[4]  = -EAGAIN,
+	[5]  = -EAGAIN,
+	[6]  = -EAGAIN,
+	[7]  = -EAGAIN,
+	[8]  = -EAGAIN,
+	[9]  = -EAGAIN,
+	[10] = -EAGAIN,
+	[11] = -EAGAIN,
+	[12] = -EAGAIN,
+	[13] = -EAGAIN,
+	[14] = -EAGAIN,
+	[15] = -EAGAIN,
 	[16] = INT_RTC,
 	[17] = INT_KBC,
 	[18] = INT_EXTERNAL_PMU,
@@ -48,50 +83,26 @@ static int tegra_wake_event_irq[] = {
 	[20] = -EINVAL, /* TEGRA_USB3_VBUS, */
 	[21] = -EINVAL, /* TEGRA_USB1_ID, */
 	[22] = -EINVAL, /* TEGRA_USB3_ID, */
-	[23] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PI5),
-	[24] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PV2),
-	[25] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PS4),
-	[26] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PS5),
-	[27] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PS0),
-	[28] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PQ6),
-	[29] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PQ7),
-	[30] = TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PN2),
+	[23] = -EAGAIN,
+	[24] = -EAGAIN,
+	[25] = -EAGAIN,
+	[26] = -EAGAIN,
+	[27] = -EAGAIN,
+	[28] = -EAGAIN,
+	[29] = -EAGAIN,
+	[30] = -EAGAIN,
 };
 
-int tegra_irq_to_wake(int irq)
+static int last_gpio = -1;
+
+int tegra_gpio_to_wake(int gpio)
 {
 	int i;
-	int wake_irq;
-	int search_gpio;
-	static int last_wake = -1;
 
-	/* Two level wake irq search for gpio based wakeups -
-	 * 1. check for GPIO irq(based on tegra_wake_event_irq table)
-	 * e.g. for a board, wake7 based on GPIO PU6 and irq==358 done first
-	 * 2. check for gpio bank irq assuming search for GPIO irq
-	 *    preceded this search.
-	 * e.g. in this step check for gpio bank irq GPIO6 irq==119
-	 */
-	for (i = 0; i < ARRAY_SIZE(tegra_wake_event_irq); i++) {
-		/* return if step 1 matches */
-		if (tegra_wake_event_irq[i] == irq) {
-			pr_info("Wake%d for irq=%d\n", i, irq);
-			last_wake = i;
-			return i;
-		}
-
-		/* step 2 below uses saved last_wake from step 1
-		 * in previous call */
-		search_gpio = tegra_wake_event_irq[i] - INT_GPIO_BASE;
-		if (search_gpio < 0)
-			continue;
-		wake_irq = tegra_gpio_get_bank_int_nr(search_gpio);
-		if (wake_irq < 0)
-			continue;
-		if ((last_wake == i) &&
-			(wake_irq == irq)) {
-			pr_info("gpio bank wake found: wake%d for irq=%d\n",
-				i, irq);
+	for (i = 0; i < ARRAY_SIZE(tegra_gpio_wakes); i++) {
+		if (tegra_gpio_wakes[i] == gpio) {
+			pr_info("gpio wake%d for gpio=%d\n", i, gpio);
+			last_gpio = i;
 			return i;
 		}
 	}
@@ -99,13 +110,55 @@ int tegra_irq_to_wake(int irq)
 	return -EINVAL;
 }
 
+int tegra_irq_to_wake(int irq)
+{
+	int i;
+	int ret = -EINVAL;
+
+	for (i = 0; i < ARRAY_SIZE(tegra_wake_event_irq); i++) {
+		if (tegra_wake_event_irq[i] == irq) {
+			pr_info("Wake %d for irq=%d\n", i, irq);
+			ret = i;
+			goto out;
+		}
+	}
+
+	/* The gpio set_wake code bubbles the set_wake call up to the irq
+	 * set_wake code. This insures that the nested irq set_wake call
+	 * succeeds, even though it doesn't have to do any pm setup for the
+	 * bank.
+	 *
+	 * This is very fragile - there's no locking, so two callers could
+	 * cause issues with this.
+	 */
+	if (last_gpio < 0)
+		goto out;
+
+	if (tegra_gpio_get_bank_int_nr(tegra_gpio_wakes[last_gpio]) == irq) {
+		pr_info("gpio bank wake found: wake %d for irq=%d\n", i, irq);
+		ret = last_gpio;
+	}
+
+out:
+	return ret;
+}
+
 int tegra_wake_to_irq(int wake)
 {
+	int ret;
+
 	if (wake < 0)
 		return -EINVAL;
 
 	if (wake >= ARRAY_SIZE(tegra_wake_event_irq))
 		return -EINVAL;
 
-	return tegra_wake_event_irq[wake];
+	ret = tegra_wake_event_irq[wake];
+	if (ret == -EAGAIN) {
+		ret = tegra_gpio_wakes[wake];
+		if (ret != -EINVAL)
+			ret = gpio_to_irq(ret);
+	}
+
+	return ret;
 }
--- drivers/gpio/gpio-tegra.c
+++ drivers/gpio/gpio-tegra.c
@@ -271,6 +271,7 @@ static int tegra_gpio_irq_set_type(struct irq_data *d, unsigned int type)
 	int lvl_type;
 	int val;
 	unsigned long flags;
+	int wake = tegra_gpio_to_wake(d->hwirq);
 
 	switch (type & IRQ_TYPE_SENSE_MASK) {
 	case IRQ_TYPE_EDGE_RISING:
@@ -311,7 +312,7 @@ static int tegra_gpio_irq_set_type(struct irq_data *d, unsigned int type)
 	else if (type & (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_EDGE_RISING))
 		__irq_set_handler_locked(d->irq, handle_edge_irq);
 
-	tegra_pm_irq_set_wake_type(d->irq, type);
+	tegra_pm_irq_set_wake_type(wake, type);
 
 	return 0;
 }
@@ -393,8 +394,9 @@ static int tegra_gpio_irq_set_wake(struct irq_data *d, unsigned int enable)
 {
 	struct tegra_gpio_bank *bank = irq_data_get_irq_chip_data(d);
 	int ret = 0;
+	int wake = tegra_gpio_to_wake(d->hwirq);
 
-	ret = tegra_pm_irq_set_wake(d->irq, enable);
+	ret = tegra_pm_irq_set_wake(wake, enable);
 
 	if (ret)
 		return ret;
@@ -402,7 +404,7 @@ static int tegra_gpio_irq_set_wake(struct irq_data *d, unsigned int enable)
 	ret = irq_set_irq_wake(bank->irq, enable);
 
 	if (ret)
-		tegra_pm_irq_set_wake(d->irq, !enable);
+		tegra_pm_irq_set_wake(wake, !enable);
 
 	return ret;
 }
