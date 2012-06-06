d0782d5b144dd85ed1e18dedda02a08918d4712d
Suresh Mangipudi <smangipudi@nvidia.com>
2012-06-06 12:08:41 +0530

    echi: tegra: Add support for unaligned buffers
    T114 supports unaligned buffers, so add support for the same.
    
    Bug 969252
    
    Change-Id: I6bd67aadec8eeb4ee3cba138fa94df4a75e736e0
    Signed-off-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-on: http://git-master/r/106688
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>

--- drivers/usb/host/ehci-tegra.c
+++ drivers/usb/host/ehci-tegra.c
@@ -54,6 +54,7 @@ struct tegra_ehci_hcd {
 	bool port_resuming;
 	unsigned int irq;
 	bool bus_suspended_fail;
+	bool unaligned_dma_buf_supported;
 };
 
 struct dma_align_buffer {
@@ -62,10 +63,14 @@ struct dma_align_buffer {
 	u8 data[0];
 };
 
-static void free_align_buffer(struct urb *urb)
+static void free_align_buffer(struct urb *urb, struct usb_hcd *hcd)
 {
 	struct dma_align_buffer *temp = container_of(urb->transfer_buffer,
 						struct dma_align_buffer, data);
+	struct tegra_ehci_hcd *tegra = dev_get_drvdata(hcd->self.controller);
+
+	if (tegra->unaligned_dma_buf_supported)
+		return;
 
 	if (!(urb->transfer_flags & URB_ALIGNED_TEMP_BUFFER))
 		return;
@@ -80,10 +85,15 @@ static void free_align_buffer(struct urb *urb)
 	kfree(temp->kmalloc_ptr);
 }
 
-static int alloc_align_buffer(struct urb *urb, gfp_t mem_flags)
+static int alloc_align_buffer(struct urb *urb, gfp_t mem_flags,
+	struct usb_hcd *hcd)
 {
 	struct dma_align_buffer *temp, *kmalloc_ptr;
 	size_t kmalloc_size;
+	struct tegra_ehci_hcd *tegra = dev_get_drvdata(hcd->self.controller);
+
+	if (tegra->unaligned_dma_buf_supported)
+		return 0;
 
 	if (urb->num_sgs || urb->sg ||
 		urb->transfer_buffer_length == 0 ||
@@ -118,7 +128,7 @@ static int tegra_ehci_map_urb_for_dma(struct usb_hcd *hcd,
 {
 	int ret;
 
-	ret = alloc_align_buffer(urb, mem_flags);
+	ret = alloc_align_buffer(urb, mem_flags, hcd);
 	if (ret)
 		return ret;
 
@@ -140,7 +150,7 @@ static int tegra_ehci_map_urb_for_dma(struct usb_hcd *hcd,
 	}
 
 	if (ret)
-		free_align_buffer(urb);
+		free_align_buffer(urb, hcd);
 
 	return ret;
 }
@@ -149,7 +159,7 @@ static void tegra_ehci_unmap_urb_for_dma(struct usb_hcd *hcd,
 	struct urb *urb)
 {
 	usb_hcd_unmap_urb_for_dma(hcd, urb);
-	free_align_buffer(urb);
+	free_align_buffer(urb, hcd);
 
 	if (urb->transfer_dma) {
 		enum dma_data_direction dir;
@@ -463,6 +473,7 @@ static int tegra_ehci_probe(struct platform_device *pdev)
 	struct resource *res;
 	struct usb_hcd *hcd;
 	struct tegra_ehci_hcd *tegra;
+	struct tegra_usb_platform_data *pdata;
 	int err = 0;
 	int irq;
 	int instance = pdev->id;
@@ -539,6 +550,9 @@ static int tegra_ehci_probe(struct platform_device *pdev)
 	}
 	set_irq_flags(irq, IRQF_VALID);
 	tegra->irq = irq;
+
+	pdata = dev_get_platdata(&pdev->dev);
+	tegra->unaligned_dma_buf_supported = pdata->unaligned_dma_buf_supported;
 
 	tegra->phy = tegra_usb_phy_open(pdev);
 	if (IS_ERR(tegra->phy)) {
