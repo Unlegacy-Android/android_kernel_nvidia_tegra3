370775fb962492c0cae833944ce4a99ae0aec40e
Sang-Hun Lee <sanlee@nvidia.com>
2012-06-15 17:15:46 -0700

    net: wireless: bcmdhd: move barrier to after setting the suspended flag.
    Reviewed-on: http://mcrd1-5.corpnet.asus/code-review/master/47846
    Tested-by: Kirt Hsieh <Kirt_Hsieh@asus.com>
    Reviewed-by: Kirt Hsieh <Kirt_Hsieh@asus.com>
    Reviewed-by: Vincent Yue <Vincent_Yue@asus.com>
    
    Bug 984811
    
    Signed-off-by: Roger Hsieh <rhsieh@nvidia.com>
    Change-Id: I708dadddf3f3eac9f8bacc72e3aef94247a87e70
    Reviewed-on: http://git-master/r/106352
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sang-Hun Lee <sanlee@nvidia.com>
    Tested-by: Sang-Hun Lee <sanlee@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

--- drivers/net/wireless/bcmdhd/bcmsdh_sdmmc_linux.c
+++ drivers/net/wireless/bcmdhd/bcmsdh_sdmmc_linux.c
@@ -2,13 +2,13 @@
  * BCMSDH Function Driver for the native SDIO/MMC driver in the Linux Kernel
  *
  * Copyright (C) 1999-2012, Broadcom Corporation
- * 
+ *
  *      Unless you and Broadcom execute a separate written software license
  * agreement governing use of this software, this software is licensed to you
  * under the terms of the GNU General Public License version 2 (the "GPL"),
  * available at http://www.broadcom.com/licenses/GPLv2.php, with the
  * following added to such license:
- * 
+ *
  *      As a special exception, the copyright holders of this software give you
  * permission to link this software with independent modules, and to copy and
  * distribute the resulting executable under terms of your choice, provided that
@@ -16,7 +16,7 @@
  * the license of that module.  An independent module is a module which is not
  * derived from this software.  The special exception does not apply to any
  * modifications of the software.
- * 
+ *
  *      Notwithstanding the above, under no circumstances may you combine this
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
@@ -186,8 +186,7 @@ static int bcmsdh_sdmmc_suspend(struct device *pdev)
 		return -EBUSY;
 #if defined(OOB_INTR_ONLY)
 	bcmsdh_oob_intr_set(0);
-#endif	/* defined(OOB_INTR_ONLY) */
-	smp_mb();
+#endif /* defined(OOB_INTR_ONLY) */
 
 	sdio_flags = sdio_get_host_pm_caps(func);
 
@@ -206,6 +205,7 @@ static int bcmsdh_sdmmc_suspend(struct device *pdev)
 	}
 
 	dhd_mmc_suspend = TRUE;
+	smp_mb();
 
 out:
 	return ret;
