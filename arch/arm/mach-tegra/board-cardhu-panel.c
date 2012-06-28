da47078147f8080a3f71d7647eb391980924d5dd
Prem Sasidharan <psasidharan@nvidia.com>
2012-06-27 18:58:45 -0700

    arm: tegra: cardhu: Change governor type
    Power improvement for OS IDLE DISPLAY OFF, audio use cases.
    Governor is changed to conservative governor when panel is
    suspended. Default governor (interactive governor) is restored
    when the panel resumes (when user presses the power button).
    
    Seeing a power improvement of 22mW (42mW/64mW) for os idle
    display off and 30mW (57mW/87mW) for audio playback use cases
    
    Bug 1002294
    Signed-off-by: Prem Sasidharan <psasidharan@nvidia.com>
    Change-Id: I06d2bca654bc8ff96fdf18d81a98c6e4ec838235
    (cherry picked from commit 7002c3fe264163b6be38cf9e24bdf137f44ed69f)
    Reviewed-on: http://git-master/r/118196
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board-cardhu-panel.c
+++ arch/arm/mach-tegra/board-cardhu-panel.c
@@ -1213,11 +1213,19 @@ static void cardhu_panel_early_suspend(struct early_suspend *h)
 		fb_blank(registered_fb[0], FB_BLANK_POWERDOWN);
 	if (num_registered_fb > 1)
 		fb_blank(registered_fb[1], FB_BLANK_NORMAL);
+
+#ifdef CONFIG_TEGRA_CONVSERVATIVE_GOV_ON_EARLYSUPSEND
+	cpufreq_store_default_gov();
+	cpufreq_change_gov(cpufreq_conservative_gov);
+#endif
 }
 
 static void cardhu_panel_late_resume(struct early_suspend *h)
 {
 	unsigned i;
+#ifdef CONFIG_TEGRA_CONVSERVATIVE_GOV_ON_EARLYSUPSEND
+	cpufreq_restore_default_gov();
+#endif
 	for (i = 0; i < num_registered_fb; i++)
 		fb_blank(registered_fb[i], FB_BLANK_UNBLANK);
 }
