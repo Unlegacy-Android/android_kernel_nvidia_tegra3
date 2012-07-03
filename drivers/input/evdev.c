21ffb6f6d01e1aaf1fc7a84cfcc44c27817b797f
Varun Wadekar <vwadekar@nvidia.com>
2012-07-03 13:39:29 +0530

    Revert "Input: Hold wake lock while event queue is not empty."
    This reverts commit a9d8fb2bb8f84568be29414184ad8e8490d38a4e since it
    breaks suspend-resume when run in a continuous loop. We have to wait
    for the timeout to occur but the wakelock does not get released ever
    when we are running suspend-resume cycles in continuous loops.
    
    Change-Id: If9bebf6d0ea06f51c0e60c2ddaf13d2be017819b
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/113239
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -63,7 +63,6 @@ static void evdev_pass_event(struct evdev_client *client,
 	/* Interrupts are disabled, just acquire the lock. */
 	spin_lock(&client->buffer_lock);
 
-	wake_lock_timeout(&client->wake_lock, 5 * HZ);
 	client->buffer[client->head++] = *event;
 	client->head &= client->bufsize - 1;
 
@@ -322,7 +321,6 @@ static int evdev_open(struct inode *inode, struct file *file)
 	spin_lock_init(&client->buffer_lock);
 	snprintf(client->name, sizeof(client->name), "%s-%d",
 			dev_name(&evdev->dev), task_tgid_vnr(current));
-	wake_lock_init(&client->wake_lock, WAKE_LOCK_SUSPEND, client->name);
 	client->evdev = evdev;
 	evdev_attach_client(evdev, client);
 
