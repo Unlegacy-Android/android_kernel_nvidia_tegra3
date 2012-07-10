78df1f241300f4c0962e01030fd0ba30ff8db3d2
Laxman Dewangan <ldewangan@nvidia.com>
2012-07-10 17:10:21 +0530

    i2c: tegra: remove unused member variable
    Remove unused member variable "iomem" of the
    i2c device structure.
    
    This variable becomes unused when converted all allocation
    to devm_* in following change:
        i2c: tegra: make all resource allocation through devm_*
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
    Cherry-picked from mainline
    	57c0dc3e69439a2ddf239226c318d676da773492
    
    Change-Id: I8a3db21524a20ee4cbd1b87dff82bac80a2763de
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/116472
    Reviewed-by: Automatic_Commit_Validation_User
    Tested-by: Bandi Krishna Chaitanya <bandik@nvidia.com>

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -162,7 +162,6 @@ struct tegra_i2c_bus {
  * @adapter: core i2c layer adapter information
  * @clk: clock reference for i2c controller
  * @i2c_clk: clock reference for i2c bus
- * @iomem: memory resource for registers
  * @base: ioremapped registers cookie
  * @cont_id: i2c controller id, used for for packet header
  * @irq: irq number of transfer complete interrupt
@@ -179,7 +178,6 @@ struct tegra_i2c_dev {
 	struct device *dev;
 	struct clk *div_clk;
 	struct clk *fast_clk;
-	struct resource *iomem;
 	struct rt_mutex dev_lock;
 	spinlock_t fifo_lock;
 	void __iomem *base;
