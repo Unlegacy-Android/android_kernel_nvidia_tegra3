afe7f3c35b4d8b8b3bed8453bf64141aafdf26ad
Bitan Biswas <bbiswas@nvidia.com>
2012-07-18 18:28:20 +0530

    rtc: set alarm logging enable
    Set alarm logs enabled to help understand suspend stress failures
    
    Change-Id: I60644ff0e3fea813ae1140be1b71fc2694d95709
    Signed-off-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-on: http://git-master/r/115361
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Jinyoung Park <jinyoungp@nvidia.com>
    Tested-by: Jinyoung Park <jinyoungp@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/rtc/alarm.c
+++ drivers/rtc/alarm.c
@@ -33,6 +33,7 @@
 #define ANDROID_ALARM_PRINT_FLOW (1U << 6)
 
 static int debug_mask = ANDROID_ALARM_PRINT_ERROR | \
+			ANDROID_ALARM_PRINT_SUSPEND | \
 			ANDROID_ALARM_PRINT_INIT_STATUS;
 module_param_named(debug_mask, debug_mask, int, S_IRUGO | S_IWUSR | S_IWGRP);
 
