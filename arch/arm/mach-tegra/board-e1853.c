c8291a86f051ce80572a451c78f529a2a6ce490a
Dongfang Shi <dshi@nvidia.com>
2012-07-27 15:58:50 +0530

    E1853 BRINGUP: HDMI bringup
    - Register HDMI device.
    - Panel init also registers HOST1x.
    - HDMI is fully functional
    - Hotplug detect GPIO, DDC bus works fine.
    
    bug 966833
    bug 1018258
    
    Change-Id: Ia04fbfb9b9ddcf81cef241f3cd1187cdc87d159d
    Reviewed-on: http://git-master/r/119118
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Raymond Rees <rrees@nvidia.com>
    Tested-by: Dongfang Shi <dshi@nvidia.com>
    (cherry picked from commit c62d307b84a034bae31afce67b8755d500170f58)
    Reviewed-on: http://git-master/r/119743
    Reviewed-by: Bob Johnston <bjohnston@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>

--- arch/arm/mach-tegra/board-e1853.c
+++ arch/arm/mach-tegra/board-e1853.c
@@ -374,9 +374,9 @@ static void __init tegra_e1853_init(void)
 static void __init tegra_e1853_reserve(void)
 {
 #if defined(CONFIG_NVMAP_CONVERT_CARVEOUT_TO_IOVMM)
-	tegra_reserve(0, SZ_8M, 0);
+	tegra_reserve(0, SZ_8M, SZ_8M);
 #else
-	tegra_reserve(SZ_128M, SZ_8M, 0);
+	tegra_reserve(SZ_128M, SZ_8M, SZ_8M);
 #endif
 }
 
