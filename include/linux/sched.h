8bd999a85354485af3cbee872816a9921d8bfffc
Sai Charan Gurrappadi <sgurrappadi@nvidia.com>
2012-08-24 18:42:36 -0700

    cpuquiet: Update averaging of nr_runnables
    Doing a Exponential moving average per nr_running++/-- does not
    guarantee a fixed sample rate which induces errors if there are lots of
    threads being enqueued/dequeued from the rq (Linpack mt). Instead of
    keeping track of the avg, the scheduler now keeps track of the integral
    of nr_running and allows the readers to perform filtering on top.
    
    Implemented a proper exponential moving average for the runnables
    governor and a straight 100ms average for the balanced governor. Tweaked
    the thresholds for the runnables governor to minimize latency. Also,
    decreased sample_rate for the runnables governor to the absolute minimum
    of 10msecs.
    
    Updated to K3.4
    
    Change-Id: Ia25bf8baf2a1a015ba188b2c06e551e89b16c5f8
    Signed-off-by: Sai Charan Gurrappadi <sgurrappadi@nvidia.com>
    Signed-off-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Reviewed-on: http://git-master/r/131147
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -139,8 +139,7 @@ extern int nr_processes(void);
 extern unsigned long nr_running(void);
 extern unsigned long nr_uninterruptible(void);
 extern unsigned long nr_iowait(void);
-extern unsigned long get_avg_nr_running(unsigned int cpu);
-extern unsigned long avg_nr_running(void);
+extern u64 nr_running_integral(unsigned int cpu);
 extern unsigned long nr_iowait_cpu(int cpu);
 extern unsigned long this_cpu_load(void);
 
