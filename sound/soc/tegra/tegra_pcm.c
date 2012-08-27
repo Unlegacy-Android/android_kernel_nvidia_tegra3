ffb7353b043f3c5336f3b2b2546e3e42ab8f2bb8
Sumit Bhattacharya <sumitb@nvidia.com>
2012-08-27 16:07:39 +0800

    ASoC: Tegra: Add ALSA ctl to set DMA address
    Add support for setting of DMA address through alsa control for AVP
    rendering. This is required to directly do DMA from IRAM to I2S.
    
    Bug 1024403
    
    Change-Id: I6b79ae6e9a562160a19d238b817e1a8b407ac208
    Signed-off-by: Sumit Bhattacharya <sumitb@nvidia.com>
    Reviewed-on: http://git-master/r/127436
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Niranjan Wartikar <nwartikar@nvidia.com>
    Reviewed-by: Ravindra Lokhande <rlokhande@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Scott Peterson <speterson@nvidia.com>

--- sound/soc/tegra/tegra_pcm.c
+++ sound/soc/tegra/tegra_pcm.c
@@ -70,7 +70,11 @@ static void tegra_pcm_queue_dma(struct tegra_runtime_data *prtd)
 	if (++prtd->dma_req_idx >= prtd->dma_req_count)
 		prtd->dma_req_idx -= prtd->dma_req_count;
 
-	addr = buf->addr + prtd->dma_pos;
+	if (prtd->avp_dma_addr)
+		addr = prtd->avp_dma_addr + prtd->dma_pos;
+	else
+		addr = buf->addr + prtd->dma_pos;
+
 	prtd->dma_pos += dma_req->size;
 	if (prtd->dma_pos >= prtd->dma_pos_end)
 		prtd->dma_pos = 0;
