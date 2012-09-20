3bca5808dcc371ea4e1d6ce555e3eae76fbe0e7e
Puneet Saxena <puneets@nvidia.com>
2012-09-20 19:22:09 +0530

    Revert "cpuquiet: Update averaging of nr_runnables"
    This reverts commit 8bd999a85354485af3cbee872816a9921d8bfffc.
    
    bug 1050721
    
    Change-Id: I29fcff431e5427dfaa2524a12c5702154037018a
    Signed-off-by: Puneet Saxena <puneets@nvidia.com>
    Reviewed-on: http://git-master/r/134307
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/cpuquiet/governors/runnable_threads.c
+++ drivers/cpuquiet/governors/runnable_threads.c
@@ -33,15 +33,14 @@ typedef enum {
 	UP,
 } RUNNABLES_STATE;
 
-static struct work_struct runnables_work;
+static struct delayed_work runnables_work;
 static struct kobject *runnables_kobject;
-static struct timer_list runnables_timer;
 
-static RUNNABLES_STATE runnables_state;
 /* configurable parameters */
-static unsigned int sample_rate = 10;		/* msec */
+static unsigned int sample_rate = 20;		/* msec */
 
 static RUNNABLES_STATE runnables_state;
+static struct workqueue_struct *runnables_wq;
 
 #define NR_FSHIFT_EXP	3
 #define NR_FSHIFT	(1 << NR_FSHIFT_EXP)
@@ -57,73 +56,25 @@ static unsigned int nr_run_thresholds[NR_CPUS];
 
 DEFINE_MUTEX(runnables_work_lock);
 
-struct runnables_avg_sample {
-	u64 previous_integral;
-	unsigned int avg;
-	bool integral_sampled;
-	u64 prev_timestamp;
-};
-
-static DEFINE_PER_CPU(struct runnables_avg_sample, avg_nr_sample);
-
-/* EXP = alpha in the exponential moving average.
- * Alpha = e ^ (-sample_rate / window_size) * FIXED_1
- * Calculated for sample_rate of 10ms, window size of 63.82ms
- */
-#define EXP    1751
-
-static unsigned int get_avg_nr_runnables(void)
-{
-	unsigned int i, sum = 0;
-	static unsigned int avg;
-	struct runnables_avg_sample *sample;
-	u64 integral, old_integral, delta_integral, delta_time, cur_time;
-
-	for_each_online_cpu(i) {
-		sample = &per_cpu(avg_nr_sample, i);
-		integral = nr_running_integral(i);
-		old_integral = sample->previous_integral;
-		sample->previous_integral = integral;
-		cur_time = ktime_to_ns(ktime_get());
-		delta_time = cur_time - sample->prev_timestamp;
-		sample->prev_timestamp = cur_time;
-
-		if (!sample->integral_sampled) {
-			sample->integral_sampled = true;
-			/* First sample to initialize prev_integral, skip
-			 * avg calculation
-			 */
-			continue;
-		}
-
-		if (integral < old_integral) {
-			/* Overflow */
-			delta_integral = (ULLONG_MAX - old_integral) + integral;
-		} else {
-			delta_integral = integral - old_integral;
-		}
-
-		/* Calculate average for the previous sample window */
-		do_div(delta_integral, delta_time);
-		sample->avg = delta_integral;
-		sum += sample->avg;
-	}
-
-	/* Exponential moving average
-	 * Avgn = Avgn-1 * alpha + new_avg * (1 - alpha)
-	 */
-	avg *= EXP;
-	avg += sum * (FIXED_1 - EXP);
-	avg >>= FSHIFT;
-
-	return avg;
-}
-
-static void update_runnables_state(unsigned int nr_run)
+static void update_runnables_state(void)
 {
 	unsigned int nr_cpus = num_online_cpus();
 	int max_cpus = pm_qos_request(PM_QOS_MAX_ONLINE_CPUS) ? : 4;
 	int min_cpus = pm_qos_request(PM_QOS_MIN_ONLINE_CPUS);
+	unsigned int avg_nr_run = avg_nr_running();
+	unsigned int nr_run;
+
+	if (runnables_state == DISABLED)
+		return;
+
+	for (nr_run = 1; nr_run < ARRAY_SIZE(nr_run_thresholds); nr_run++) {
+		unsigned int nr_threshold = nr_run_thresholds[nr_run - 1];
+		if (nr_run_last <= nr_run)
+			nr_threshold += NR_FSHIFT / nr_run_hysteresis;
+		if (avg_nr_run <= (nr_threshold << (FSHIFT - NR_FSHIFT_EXP)))
+			break;
+	}
+	nr_run_last = nr_run;
 
 	if ((nr_cpus > max_cpus || nr_run < nr_cpus) && nr_cpus >= min_cpus) {
 		runnables_state = DOWN;
@@ -134,31 +85,6 @@ static void update_runnables_state(unsigned int nr_run)
 	}
 }
 
-static void runnables_avg_sampler(unsigned long data)
-{
-	unsigned int nr_run, avg_nr_run;
-
-	if (runnables_state == DISABLED)
-		return;
-
-	avg_nr_run = get_avg_nr_runnables();
-	mod_timer(&runnables_timer, jiffies + msecs_to_jiffies(sample_rate));
-
-	for (nr_run = 1; nr_run < ARRAY_SIZE(nr_run_thresholds); nr_run++) {
-		unsigned int nr_threshold = nr_run_thresholds[nr_run - 1];
-		if (nr_run_last <= nr_run)
-			nr_threshold += NR_FSHIFT / nr_run_hysteresis;
-		if (avg_nr_run <= (nr_threshold << (FSHIFT - NR_FSHIFT_EXP)))
-			break;
-	}
-
-	nr_run_last = nr_run;
-	update_runnables_state(nr_run);
-
-	if (runnables_state != DISABLED && runnables_state != IDLE)
-		schedule_work(&runnables_work);
-}
-
 static unsigned int get_lightest_loaded_cpu_n(void)
 {
 	unsigned long min_avg_runnables = ULONG_MAX;
@@ -166,8 +92,8 @@ static unsigned int get_lightest_loaded_cpu_n(void)
 	int i;
 
 	for_each_online_cpu(i) {
-		struct runnables_avg_sample *s = &per_cpu(avg_nr_sample, i);
-		unsigned int nr_runnables = s->avg;
+		unsigned int nr_runnables = get_avg_nr_running(i);
+
 		if (i > 0 && min_avg_runnables > nr_runnables) {
 			cpu = i;
 			min_avg_runnables = nr_runnables;
@@ -180,29 +106,37 @@ static unsigned int get_lightest_loaded_cpu_n(void)
 static void runnables_work_func(struct work_struct *work)
 {
 	bool up = false;
+	bool sample = false;
 	unsigned int cpu = nr_cpu_ids;
 
 	mutex_lock(&runnables_work_lock);
 
-	/* Update state to avoid duplicate operations */
-	update_runnables_state(nr_run_last);
+	update_runnables_state();
 
 	switch (runnables_state) {
 	case DISABLED:
+		break;
 	case IDLE:
+		sample = true;
 		break;
 	case UP:
 		cpu = cpumask_next_zero(0, cpu_online_mask);
 		up = true;
+		sample = true;
 		break;
 	case DOWN:
 		cpu = get_lightest_loaded_cpu_n();
+		sample = true;
 		break;
 	default:
 		pr_err("%s: invalid cpuquiet runnable governor state %d\n",
 			__func__, runnables_state);
 		break;
 	}
+
+	if (sample)
+		queue_delayed_work(runnables_wq, &runnables_work,
+					msecs_to_jiffies(sample_rate));
 
 	if (cpu < nr_cpu_ids) {
 		if (up)
@@ -256,7 +190,7 @@ static void runnables_device_busy(void)
 {
 	if (runnables_state != DISABLED) {
 		runnables_state = DISABLED;
-		cancel_work_sync(&runnables_work);
+		cancel_delayed_work_sync(&runnables_work);
 	}
 }
 
@@ -264,14 +198,15 @@ static void runnables_device_free(void)
 {
 	if (runnables_state == DISABLED) {
 		runnables_state = IDLE;
-		mod_timer(&runnables_timer, jiffies + 1);
+		runnables_work_func(NULL);
 	}
 }
 
 static void runnables_stop(void)
 {
 	runnables_state = DISABLED;
-	cancel_work_sync(&runnables_work);
+	cancel_delayed_work_sync(&runnables_work);
+	destroy_workqueue(runnables_wq);
 	kobject_put(runnables_kobject);
 }
 
@@ -283,10 +218,12 @@ static int runnables_start(void)
 	if (err)
 		return err;
 
-	INIT_WORK(&runnables_work, runnables_work_func);
+	runnables_wq = alloc_workqueue("cpuquiet-runnables",
+			WQ_UNBOUND | WQ_RESCUER | WQ_FREEZABLE, 1);
+	if (!runnables_wq)
+		return -ENOMEM;
 
-	init_timer(&runnables_timer);
-	runnables_timer.function = runnables_avg_sampler;
+	INIT_DELAYED_WORK(&runnables_work, runnables_work_func);
 
 	for(i = 0; i < ARRAY_SIZE(nr_run_thresholds); ++i) {
 		if (i < ARRAY_SIZE(default_thresholds))
@@ -299,7 +236,7 @@ static int runnables_start(void)
 	}
 
 	runnables_state = IDLE;
-	runnables_avg_sampler(0);
+	runnables_work_func(NULL);
 
 	return 0;
 }
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2161,10 +2161,35 @@ unsigned long this_cpu_load(void)
 	return this->cpu_load[0];
 }
 
-u64 nr_running_integral(unsigned int cpu)
+unsigned long avg_nr_running(void)
 {
-	unsigned int seqcnt;
-	u64 integral;
+	unsigned long i, sum = 0;
+	unsigned int seqcnt, ave_nr_running;
+
+	for_each_online_cpu(i) {
+		struct rq *q = cpu_rq(i);
+
+		/*
+		 * Update average to avoid reading stalled value if there were
+		 * no run-queue changes for a long time. On the other hand if
+		 * the changes are happening right now, just read current value
+		 * directly.
+		 */
+		seqcnt = read_seqcount_begin(&q->ave_seqcnt);
+		ave_nr_running = do_avg_nr_running(q);
+		if (read_seqcount_retry(&q->ave_seqcnt, seqcnt)) {
+			read_seqcount_begin(&q->ave_seqcnt);
+			ave_nr_running = q->ave_nr_running;
+		}
+
+		sum += ave_nr_running;
+	}
+
+	return sum;
+}
+
+unsigned long get_avg_nr_running(unsigned int cpu)
+{
 	struct rq *q;
 
 	if (cpu >= nr_cpu_ids)
@@ -2172,21 +2197,7 @@ u64 nr_running_integral(unsigned int cpu)
 
 	q = cpu_rq(cpu);
 
-	/*
-	 * Update average to avoid reading stalled value if there were
-	 * no run-queue changes for a long time. On the other hand if
-	 * the changes are happening right now, just read current value
-	 * directly.
-	 */
-
-	seqcnt = read_seqcount_begin(&q->ave_seqcnt);
-	integral = do_nr_running_integral(q);
-	if (read_seqcount_retry(&q->ave_seqcnt, seqcnt)) {
-		read_seqcount_begin(&q->ave_seqcnt);
-		integral = q->nr_running_integral;
-	}
-
-	return integral;
+	return q->ave_nr_running;
 }
 
 /*
