49d3d239f59683062bd78d2a4de67634a92069da
Wei Ni <wni@nvidia.com>
2012-09-26 15:58:18 +0800

    brcmfmac: Handling IRQ in ISR directly for non-OOB
    In case of inband interrupts, if we handle the interrupt in dpc thread,
    two level of thread switching takes place to process wifi interrupts.
    One in SDHCI driver and the other in Wifi driver. This may cause the system
    instability.
    Because the SDHCI calls sdio_irq_thread() to handle the irq, this thread locks
    mmc host and calls wifi handler. It expects WiFi handler to be quick and
    enables sdio interrupt from card at end. If wifi handler defers this work for
    a different thread, sdio_irq_thread() will be stuck on next wifi interrupt
    since mmc lock is not freed.
    
    Handling the interrupt in ISR directly will prevent thread context switching
    in wifi driver. It can fix the instability problems.
    
    Change-Id: I1d0208f09d16d01a154d446f52f24356ebf7cddc
    Signed-off-by: Wei Ni <wni@nvidia.com>
    Reviewed-on: http://git-master/r/142591
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- drivers/net/wireless/brcm80211/brcmfmac/dhd_sdio.c
+++ drivers/net/wireless/brcm80211/brcmfmac/dhd_sdio.c
@@ -3601,11 +3601,16 @@ void brcmf_sdbrcm_isr(void *arg)
 	if (!bus->intr)
 		brcmf_dbg(ERROR, "isr w/o interrupt configured!\n");
 
+#ifndef CONFIG_BRCMFMAC_SDIO_OOB
+	while (brcmf_sdbrcm_dpc(bus))
+		;
+#else
 	bus->dpc_sched = true;
 	if (bus->dpc_tsk) {
 		brcmf_sdbrcm_adddpctsk(bus);
 		complete(&bus->dpc_wait);
 	}
+#endif
 }
 
 static bool brcmf_sdbrcm_bus_watchdog(struct brcmf_sdio *bus)
