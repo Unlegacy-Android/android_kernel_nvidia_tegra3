reocat/msm-5.4

Input: atmel_mxt_ts - Bootloader addresses for mXT1664/mXT1188S
The mXT1664S family chips use different mappings for bootloader addresses.

Signed-off-by: Nick Dyer <nick.dyer@itdev.co.uk>

drivers/input/touchscreen/atmel_mxt_ts.c
@@ -339,12 +339,17 @@ struct mxt_i2c_address_pair {
 };
 
 static const struct mxt_i2c_address_pair mxt_i2c_addresses[] = {
+#ifdef BOOTLOADER_1164_1188
+	{ 0x26, 0x4a },
+	{ 0x27, 0x4b },
+#else
 	{ 0x24, 0x4a },
 	{ 0x25, 0x4b },
 	{ 0x26, 0x4c },
 	{ 0x27, 0x4d },
 	{ 0x34, 0x5a },
 	{ 0x35, 0x5b },
+#endif
 };
 
 static int mxt_bootloader_read(struct mxt_data *data, u8 *val, unsigned int count)

Unlegacy-Android/android_kernel_nvidia_tegra3

drivers/input/touchscreen/atmel_mxt_ts.c
@@ -339,12 +339,17 @@ struct mxt_i2c_address_pair {
 };
 
 static const struct mxt_i2c_address_pair mxt_i2c_addresses[] = {
+#ifdef BOOTLOADER_1664_1188
+	{ 0x26, 0x4a },
+	{ 0x27, 0x4b },
+#else
 	{ 0x24, 0x4a },
 	{ 0x25, 0x4b },
 	{ 0x26, 0x4c },
 	{ 0x27, 0x4d },
 	{ 0x34, 0x5a },
 	{ 0x35, 0x5b },
+#endif
 };
 
 static int mxt_bootloader_read(struct mxt_data *data, u8 *val, unsigned int count)