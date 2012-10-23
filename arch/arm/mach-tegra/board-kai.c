0456679d9c683b15606218bbcd3a4f017ce77613
Jiukai Ma <jiukaim@nvidia.com>
2012-10-23 15:32:43 +0800

    arm: tegra: kai: add tegra wakeup monitor support
    Add the platform data of tegra wakeup monitor to enable it.
    
    Bug 890577
    
    Change-Id: I8f76b45f20ab1908cc9d03a7799216588511e099
    Signed-off-by: Jiukai Ma <jiukaim@nvidia.com>
    Reviewed-on: http://git-master/r/146773
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- arch/arm/mach-tegra/board-kai.c
+++ arch/arm/mach-tegra/board-kai.c
@@ -63,6 +63,7 @@
 #include <mach/usb_phy.h>
 #include <mach/gpio-tegra.h>
 #include <mach/tegra_fiq_debugger.h>
+#include <mach/tegra_wakeup_monitor.h>
 
 #include "board.h"
 #include "board-common.h"
@@ -456,6 +457,19 @@ static struct platform_device tegra_rtc_device = {
 	.num_resources = ARRAY_SIZE(tegra_rtc_resources),
 };
 
+static struct tegra_wakeup_monitor_platform_data
+					kai_tegra_wakeup_monitor_pdata = {
+	.wifi_wakeup_source	= 1,  /* kai's wifi wakeup source */
+};
+
+static struct platform_device kai_tegra_wakeup_monitor_device = {
+	.name = "tegra_wakeup_monitor",
+	.id   = -1,
+	.dev  = {
+		.platform_data = &kai_tegra_wakeup_monitor_pdata,
+	},
+};
+
 static struct tegra_asoc_platform_data kai_audio_pdata = {
 	.gpio_spkr_en		= TEGRA_GPIO_SPKR_EN,
 	.gpio_hp_det		= TEGRA_GPIO_HP_DET,
@@ -535,6 +549,9 @@ static struct platform_device *kai_devices[] __initdata = {
 	&spdif_dit_device,
 	&bluetooth_dit_device,
 	&tegra_pcm_device,
+#if defined(CONFIG_TEGRA_WAKEUP_MONITOR)
+	&kai_tegra_wakeup_monitor_device,
+#endif
 	&kai_audio_device,
 	&kai_leds_gpio_device,
 	&tegra_hda_device,
