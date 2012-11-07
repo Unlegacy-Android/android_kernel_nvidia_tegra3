8da7eff749cdf2e2b532b0ac2030f5647098fad7
Puneet Saxena <puneets@nvidia.com>
2012-11-07 12:02:44 +0530

    Arm: kernel: remove printk for improving hotplug latency
    It removes verbose prints for improving hotplug
    latency.
    
    bug 1040355
    
    Change-Id: I646c6aba8133dc451a9b217174ccf810256ff101
    Signed-off-by: Puneet Saxena <puneets@nvidia.com>
    Reviewed-on: http://git-master/r/161900
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Antti Miettinen <amiettinen@nvidia.com>
    Tested-by: Antti Miettinen <amiettinen@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -270,7 +270,6 @@ void __cpuinit __cpu_die(unsigned int cpu)
 		pr_err("CPU%u: cpu didn't die\n", cpu);
 		return;
 	}
-	printk(KERN_NOTICE "CPU%u: shutdown\n", cpu);
 
 	if (!platform_cpu_kill(cpu))
 		printk("CPU%u: unable to kill\n", cpu);
@@ -372,7 +371,6 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	 */
 	set_cpu_online(cpu, true);
 	complete(&cpu_running);
-	printk("CPU%u: Booted secondary processor\n", cpu);
 
 	/*
 	 * Setup the percpu timer for this CPU.
