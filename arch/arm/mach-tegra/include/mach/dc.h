e8b871d405cc73c43b3d65b589df049f9ece1c8e
Kevin Huang <kevinh@nvidia.com>
2012-12-28 17:11:52 -0800

    video: tegra: hdmi: support custom tmds config.
    provide the support to set the tmds config according to board file.
    
    Bug 1204008
    
    Signed-off-by: Kevin Huang <kevinh@nvidia.com>
    Reviewed-on: http://git-master/r/187553
    (cherry picked from 70206460df252616577b0a0a69884becb66c999d)
    Change-Id: I1952fab46f60ef740a49786605390b9bb4772704
    Reviewed-on: http://git-master/r/221315
    Tested-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -485,6 +485,7 @@ struct tegra_dc_out {
 	int				n_modes;
 
 	struct tegra_dsi_out		*dsi;
+	struct tegra_hdmi_out		*hdmi_out;
 	struct tegra_stereo_out		*stereo;
 
 	unsigned			height; /* mm */
@@ -767,4 +768,19 @@ void tegra_log_resume_time(void);
 #else
 #define tegra_log_resume_time()
 #endif
+
+/* table of electrical settings, must be in acending order. */
+struct tdms_config {
+	int pclk;
+	u32 pll0;
+	u32 pll1;
+	u32 pe_current; /* pre-emphasis */
+	u32 drive_current;
+	u32 peak_current; /* for TEGRA_11x_SOC */
+};
+
+struct tegra_hdmi_out {
+	struct tdms_config *tdms_config;
+	int n_tdms_config;
+};
 #endif
