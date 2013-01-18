1c94ab3f1e1a0d1f2aef8e5847587526be4231f5
Alex Frid <afrid@nvidia.com>
2013-01-18 12:31:22 -0800

    ARM: tegra11: clock: Add direct access to CPU backup source
    Added CPU backup source access API for CPU idle driver to directly
    manipulate rate underneath cpufreq governor.
    
    Change-Id: I837e591148c5ea82e036c6b236c115690313d2c2
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/192490
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- arch/arm/mach-tegra/cpuidle.h
+++ arch/arm/mach-tegra/cpuidle.h
@@ -40,6 +40,7 @@ struct tegra_cpuidle_ops {
 int tegra2_cpuidle_init_soc(struct tegra_cpuidle_ops *ops);
 int tegra3_cpuidle_init_soc(struct tegra_cpuidle_ops *ops);
 int tegra11x_cpuidle_init_soc(struct tegra_cpuidle_ops *ops);
+int tegra11_cpu_backup_rate_exchange(unsigned long *rate);
 
 static inline int tegra_cpuidle_init_soc(struct tegra_cpuidle_ops *ops)
 {
@@ -51,6 +52,15 @@ static inline int tegra_cpuidle_init_soc(struct tegra_cpuidle_ops *ops)
 #endif
 #ifdef CONFIG_ARCH_TEGRA_11x_SOC
 	return tegra11x_cpuidle_init_soc(ops);
+#endif
+}
+
+static inline int tegra_cpu_backup_rate_exchange(unsigned long *rate)
+{
+#ifdef CONFIG_ARCH_TEGRA_11x_SOC
+	return tegra11_cpu_backup_rate_exchange(rate);
+#else
+	return -ENOSYS;
 #endif
 }
 
