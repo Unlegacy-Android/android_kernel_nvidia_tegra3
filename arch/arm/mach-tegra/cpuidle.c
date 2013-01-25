d51f612d098328a464278afbafb12c83d7cd411b
Bo Yan <byan@nvidia.com>
2013-01-25 13:55:24 -0800

    ARM: tegra: Remove redundant tegra_idle_device
    cpuidle.c in drivers/cpuidle already defines per-CPU variable
    cpuidle_devices, which holds registered devices for each CPU.
    The static tegra_idle_device is unnecessary.
    
    Change-Id: I471515b8d582fc25f5cec0f2d78b2db14b064c4e
    Signed-off-by: Bo Yan <byan@nvidia.com>
    Reviewed-on: http://git-master/r/194267
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User

--- arch/arm/mach-tegra/cpuidle.c
+++ arch/arm/mach-tegra/cpuidle.c
@@ -3,7 +3,7 @@
  *
  * CPU idle driver for Tegra CPUs
  *
- * Copyright (c) 2010-2012, NVIDIA Corporation.
+ * Copyright (c) 2010-2013, NVIDIA Corporation.
  * Copyright (c) 2011 Google, Inc.
  * Author: Colin Cross <ccross@android.com>
  *         Gary King <gking@nvidia.com>
@@ -63,8 +63,6 @@ struct cpuidle_driver tegra_idle_driver = {
 	.name = "tegra_idle",
 	.owner = THIS_MODULE,
 };
-
-static DEFINE_PER_CPU(struct cpuidle_device *, tegra_idle_device);
 
 static int tegra_idle_enter_clock_gating(struct cpuidle_device *dev,
 	int index)
@@ -209,7 +207,6 @@ static int tegra_cpuidle_register_device(unsigned int cpu)
 		kfree(dev);
 		return -EIO;
 	}
-	per_cpu(tegra_idle_device, cpu) = dev;
 	return 0;
 }
 
