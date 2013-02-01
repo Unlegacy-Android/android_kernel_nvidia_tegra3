36f80449645b88b07d4d3b115eca154d075fe4fd
David Jung <djung@nvidia.com>
2013-02-01 13:16:12 -0800

    input: touch: raydium remove spurious interrupts
    Raydium code drop to remove spurious interrupts.
    
    Bug 1219704
    
    Change-Id: I03d0c178b1d788753026b55dceebf2175484f983
    Signed-off-by: David Jung <djung@nvidia.com>
    Reviewed-on: http://git-master/r/196560
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/input/touchscreen/rm31080a_ts.c
+++ drivers/input/touchscreen/rm31080a_ts.c
@@ -1,7 +1,7 @@
 /*
  * Raydium RM31080 touchscreen driver
  *
- * Copyright (C) 2012 - 2013, Raydium Semiconductor Corporation
+ * Copyright (C) 2012 - 2013, Raydium Semiconductor Corporation.
  * Copyright (C) 2012 - 2013, NVIDIA Corporation, All Rights Reserved.
  *
  * This program is free software; you can redistribute  it and/or modify it
@@ -698,7 +698,8 @@ void rm31080_ctrl_enter_auto_mode(void)
 	/*Enable auto scan */
 	if (g_stCtrl.bfIdleMessage)
 		rm_printk("Enter Auto Scan Mode\n");
-
+	/*Set idle*/
+	rm_set_idle(1);
 	if (g_stCtrl.bSTScan)
 		rm_set_auto(1);
 	else
@@ -712,7 +713,8 @@ void rm31080_ctrl_leave_auto_mode(void)
 
 	if (g_stCtrl.bfIdleMessage)
 		rm_printk("Leave Auto Scan Mode\n");
-
+	/*leave idle*/
+	rm_set_idle(0);
 	if (g_stCtrl.bSTScan)
 		rm_set_auto(0);
 	else
