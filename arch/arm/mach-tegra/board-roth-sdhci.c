f08420c0531a0a46f4e9f3be52ab2c2e08ec3318
Pavan Kunapuli <pkunapuli@nvidia.com>
2013-02-01 22:41:34 +0530

    ARM: tegra: roth: limit eMMC clock to 156MHz
    Limiting eMMC clock to 156MHz in HS200 mode.
    Passing nominal core voltage value through platform data to be used
    during tuning for HS200 mode.
    
    Bug 1225343
    
    Change-Id: I1ff4065db505a2b3022a1d9fab10a05c5f3a1a31
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/196485
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- arch/arm/mach-tegra/board-roth-sdhci.c
+++ arch/arm/mach-tegra/board-roth-sdhci.c
@@ -172,6 +172,7 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data3 = {
 	.tap_delay = 0x5,
 	.trim_delay = 0,
 	.ddr_clk_limit = 41000000,
+	.max_clk_limit = 156000000,
 	.mmc_data = {
 		.built_in = 1,
 	}
@@ -415,9 +416,12 @@ int __init roth_sdhci_init(void)
 	int nominal_core_mv;
 
 	nominal_core_mv = tegra_dvfs_rail_get_nominal_millivolts(tegra_core_rail);
-	if (nominal_core_mv > 0)
+	if (nominal_core_mv > 0) {
 		tegra_sdhci_platform_data0.nominal_vcore_uV = nominal_core_mv *
 			1000;
+		tegra_sdhci_platform_data3.nominal_vcore_uV = nominal_core_mv *
+			1000;
+	}
 	platform_device_register(&tegra_sdhci_device3);
 	platform_device_register(&tegra_sdhci_device2);
 	platform_device_register(&tegra_sdhci_device0);
