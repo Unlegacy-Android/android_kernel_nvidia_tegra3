ccc79951c26bbd0dedbb0b72e9442112db2bc37c
Joshua Cha <joshuac@nvidia.com>
2013-02-14 18:44:37 +0900

    ARM: tegra: macallan: Upgrade CL-DVFS disable output ops
    Bug 1235511
    
    Change-Id: I18eb9f077a2ae0c3ccf73f450e3831acdc3bd11c
    Signed-off-by: Joshua Cha <joshuac@nvidia.com>
    Reviewed-on: http://git-master/r/200731
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- arch/arm/mach-tegra/board-macallan-power.c
+++ arch/arm/mach-tegra/board-macallan-power.c
@@ -37,6 +37,7 @@
 
 #include <mach/iomap.h>
 #include <mach/irqs.h>
+#include <mach/hardware.h>
 #include <mach/edp.h>
 #include <mach/gpio-tegra.h>
 
@@ -559,6 +560,8 @@ static struct tegra_cl_dvfs_platform_data macallan_cl_dvfs_data = {
 static int __init macallan_cl_dvfs_init(void)
 {
 	fill_reg_map();
+	if (tegra_revision < TEGRA_REVISION_A02)
+		macallan_cl_dvfs_data.out_quiet_then_disable = true;
 	tegra_cl_dvfs_device.dev.platform_data = &macallan_cl_dvfs_data;
 	platform_device_register(&tegra_cl_dvfs_device);
 
