e11ccb30b44fc55ba0576f5082e5e17e9a1d1854
Catalin Marinas <catalin.marinas@arm.com>
2013-02-19 16:34:29 +0000

    arm: errata: Workaround for Cortex-A15 erratum 798181 (TLBI/DSB operations)
    On Cortex-A15 (r0p0..r3p2) the TLBI/DSB are not adequately shooting down
    all use of the old entries. This patch implements the erratum workaround
    which consists of:
    
    1. Dummy TLBIMVAIS and DSB on the CPU doing the TLBI operation.
    2. Send IPI to the CPUs that are running the same mm (and ASID) as the
       one being invalidated (or all the online CPUs for global pages).
    3. CPU receiving the IPI executes a DMB and CLREX (part of the exception
       return code already).
    
    The switch_mm() code includes a DMB operation since the IPI is only sent
    to CPUs running the same ASID.
    
    Change-Id: Ideb7f479910f7d4bf25182c84eb5e71691c42a93
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Bo Yan <byan@nvidia.com>
    Reviewed-on: http://git-master/r/209830
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/include/asm/mmu_context.h
+++ arch/arm/include/asm/mmu_context.h
@@ -125,6 +125,13 @@ switch_mm(struct mm_struct *prev, struct mm_struct *next,
 #endif
 		check_context(next);
 		cpu_switch_mm(next->pgd, next);
+#ifdef CONFIG_ARM_ERRATA_798181
+		/*
+		 * DMB required since we only send IPI to the other CPUs if
+		 * they run the same mm as the one being invalidated.
+		 */
+		dmb();
+#endif
 		if (cache_is_vivt())
 			cpumask_clear_cpu(cpu, mm_cpumask(prev));
 	}
