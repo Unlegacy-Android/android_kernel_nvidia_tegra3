2ac5b251280f85313e891bec97c92d624144cbc0
Alex Frid <afrid@nvidia.com>
2013-03-11 21:58:05 -0700

    ARM: tegra: dvfs: Add VDD_CORE override mechanism
    Added mechanism to fix and lock (override) VDD_CORE rail voltage at
    particular level. When override mode is entered, all scalable shared
    buses and bus users (memory, graphics cbus clocks, system bus clocks)
    are throttled to/below the rate safe at override voltage. Other clocks,
    however, continue to run at rates set by the respective drivers. Hence
    override voltage must be high enough to allow maximum rates of all core
    clocks outside shared buses.
    
    The lowest possible override level is determined by core dvfs tables,
    and the supported override range is defined accordingly. Attempt to
    set override voltage outside the range will fail, with the exception of
    level 0 that is interpreted as request to exit override mode.
    
    Override voltage cannot be changed if VDD_CORE is already locked: first
    override mode has to be exited via zero level request, and then a new
    override voltage can be set. No other override arbitration is provided.
    
    Bug 1246712
    
    Change-Id: I56e0dd250364c87b4036a07e6cdca750f556d008
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/209395
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- arch/arm/mach-tegra/include/mach/clk.h
+++ arch/arm/mach-tegra/include/mach/clk.h
@@ -40,6 +40,7 @@ void tegra_periph_reset_deassert(struct clk *c);
 void tegra_periph_reset_assert(struct clk *c);
 
 int tegra_dvfs_set_rate(struct clk *c, unsigned long rate);
+int tegra_dvfs_override_core_voltage(int override_mv);
 unsigned long clk_get_rate_all_locked(struct clk *c);
 
 #ifdef CONFIG_ARCH_TEGRA_2x_SOC
