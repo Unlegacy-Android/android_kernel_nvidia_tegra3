3da8fdb7dfe08683b74803c914e5a85cc2a86fca
Kerwin Wan <kerwinw@nvidia.com>
2013-03-14 13:00:24 +0800

    ARM: tegra: macallan: limit sdmmc1, sdmmc3 max clock
    CRC errors are seen in higher clock rates.
    limiting sdmmc1, sdmmc3 max clock to 82 MHZ.
    
    Bug 1189241
    
    Change-Id: I36e26f93c215ecfa019e5e332898b47b31d2adf7
    Signed-off-by: Kerwin Wan <kerwinw@nvidia.com>
    Reviewed-on: http://git-master/r/209424
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

--- arch/arm/mach-tegra/board-macallan-sdhci.c
+++ arch/arm/mach-tegra/board-macallan-sdhci.c
@@ -127,6 +127,7 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data0 = {
 	.tap_delay = 0x2,
 	.trim_delay = 0x2,
 	.ddr_clk_limit = 41000000,
+	.max_clk_limit = 82000000,
 	.uhs_mask = MMC_UHS_MASK_SDR104 |
 		MMC_UHS_MASK_DDR50,
 };
@@ -138,6 +139,7 @@ static struct tegra_sdhci_platform_data tegra_sdhci_platform_data2 = {
 	.tap_delay = 0x3,
 	.trim_delay = 0x3,
 	.ddr_clk_limit = 41000000,
+	.max_clk_limit = 82000000,
 };
 
 static struct tegra_sdhci_platform_data tegra_sdhci_platform_data3 = {
