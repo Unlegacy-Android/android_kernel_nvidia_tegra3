59d3dfbd57e01074b04d6971a74f16fec6281d75
Pradeep Goudagunta <pgoudagunta@nvidia.com>
2013-04-04 12:03:00 -0700

    power: bq2419x: Update current limit to 2.25A
    Bug 1224377
    
    Conflicts:
    	drivers/power/bq2419x-charger.c
    
    Change-Id: I19c942cfb86eead8e6bc2d40f9f3b909cd22d9f2
    Signed-off-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-on: http://git-master/r/216611
    (cherry picked from commit daee67693dc847feadb4f96d995db78f679faebd)
    Reviewed-on: http://git-master/r/228314
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User

--- drivers/power/bq2419x-charger.c
+++ drivers/power/bq2419x-charger.c
@@ -256,8 +256,8 @@ static int bq2419x_charger_init(struct bq2419x_chip *bq2419x)
 {
 	int ret;
 
-	/* Configure Output Current Control to 3A*/
-	ret = regmap_write(bq2419x->regmap, BQ2419X_CHRG_CTRL_REG, 0xC0);
+	/* Configure Output Current Control to 2.25A*/
+	ret = regmap_write(bq2419x->regmap, BQ2419X_CHRG_CTRL_REG, 0x6c);
 	if (ret < 0) {
 		dev_err(bq2419x->dev, "CHRG_CTRL_REG write failed %d\n", ret);
 		return ret;
