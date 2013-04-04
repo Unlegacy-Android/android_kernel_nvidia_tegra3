d93ba8bd5d993d51024571a25a2d9d1649752bbf
Pavan Kunapuli <pkunapuli@nvidia.com>
2013-04-04 17:52:31 +0530

    mmc: sdio: Register for device frequency scaling
    If CONFIG_MMC_FREQ_SCALING is enabled, register for device frequency
    scaling.
    
    Bug 1238045
    
    Change-Id: I11bb807a4658246b18fa41b4e59c63271c5c00f0
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/213013
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Naveen Kumar Arepalli <naveenk@nvidia.com>
    Tested-by: Naveen Kumar Arepalli <naveenk@nvidia.com>
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -828,6 +828,16 @@ static int mmc_sdio_init_card(struct mmc_host *host, u32 ocr,
 finish:
 	if (!oldcard)
 		host->card = card;
+
+#ifdef CONFIG_MMC_FREQ_SCALING
+	/*
+	 * This implementation is still in experimental phase. So, don't fail
+	 * enumeration even if dev freq init fails.
+	 */
+	if (mmc_devfreq_init(host))
+		dev_info(mmc_dev(host),
+			"Device frequency scaling initialization failed %d\n", err);
+#endif
 	return 0;
 
 remove:
@@ -854,6 +864,10 @@ static void mmc_sdio_remove(struct mmc_host *host)
 			host->card->sdio_func[i] = NULL;
 		}
 	}
+
+#ifdef CONFIG_MMC_FREQ_SCALING
+	mmc_devfreq_deinit(host);
+#endif
 
 	mmc_remove_card(host->card);
 	host->card = NULL;
