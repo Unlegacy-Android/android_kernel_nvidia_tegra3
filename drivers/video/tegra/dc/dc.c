28199be1263b88070718bac2725e8d5b26c6a0f6
Kamal Kannan Balagopalan <kbalagopalan@nvidia.com>
2013-04-09 17:39:15 -0700

    video: tegra: dc: Log display-on time via uS timer
    Bug 1252226
    
    Change-Id: I0c7486813c6682a12d79fdfcddc4a47799794ab6
    Signed-off-by: Kamal Kannan Balagopalan <kbalagopalan@nvidia.com>
    Reviewed-on: http://git-master/r/217940
    Reviewed-by: Xin Xie <xxie@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Li Li (SW-TEGRA) <lli5@nvidia.com>
    Reviewed-by: Venkata (Muni) Anda <vanda@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -1834,12 +1834,8 @@ static bool _tegra_dc_controller_enable(struct tegra_dc *dc)
 
 	tegra_dc_unpowergate_locked(dc);
 
-	if (dc->out->enable) {
+	if (dc->out->enable)
 		dc->out->enable(&dc->ndev->dev);
-
-		/* important to track dc init latency */
-		dev_info(&dc->ndev->dev, "dc out enabled\n");
-	}
 
 	tegra_dc_setup_clk(dc, dc->clk);
 	tegra_dc_clk_enable(dc);
@@ -1879,6 +1875,8 @@ static bool _tegra_dc_controller_enable(struct tegra_dc *dc)
 
 	if (dc->out->postpoweron)
 		dc->out->postpoweron();
+
+	tegra_log_resume_time();
 
 	tegra_dc_io_end(dc);
 	return true;
