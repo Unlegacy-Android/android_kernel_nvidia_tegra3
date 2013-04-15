eaf4da5d9d1c1cb165a6e563bacee25e19f9af84
Ilan Aelion <iaelion@nvidia.com>
2013-04-15 17:02:39 -0600

    video: tegra: host: fix 3d.emc calculation
    Bug 1268909
    
    Change-Id: I04f5ae2b4b6138bbca0306b0f76c0b763c018b74
    Signed-off-by: Ilan Aelion <iaelion@nvidia.com>
    Reviewed-on: http://git-master/r/219565
    Reviewed-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>

--- drivers/video/tegra/host/gr3d/scale3d.h
+++ drivers/video/tegra/host/gr3d/scale3d.h
@@ -42,4 +42,17 @@ void nvhost_scale3d_deinit(struct platform_device *);
 void nvhost_scale3d_notify_busy(struct platform_device *);
 void nvhost_scale3d_notify_idle(struct platform_device *);
 
+/*
+ * 20.12 fixed point arithmetic
+ */
+#define INT_TO_FX(x) ((x) << 12)
+#define FX_TO_INT(x) ((x) >> 12)
+int FXMUL(unsigned int x, unsigned int y);
+int FXDIV(unsigned int x, unsigned int y);
+
+#define FX_HALF 0x800
+
+#define MHZ_TO_HZ(x) ((x) * 1000000)
+#define HZ_TO_MHZ(x) ((x) / 1000000)
+
 #endif
