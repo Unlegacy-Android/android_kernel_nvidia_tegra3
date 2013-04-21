01de8fbcaec740ad6e5337562df3d0ad29b78eab
Hunk Lin <hulin@nvidia.com>
2013-04-21 16:28:57 +0800

    ARM: tegra: macallan/tegratab: Clear fb2 if unspecified
    Clear framebuffer2 if and only if framebuffer2 is not specified by the
    bootloader. If the bootloader framebuffer2 is specified, then copy the
    contents to kernel framebuffer2.
    
    Bug 1229981
    
    Change-Id: I7eb6fc121e2f1e3d81394e3a8620ac5d370d76f2
    Signed-off-by: Hunk Lin <hulin@nvidia.com>
    Reviewed-on: http://git-master/r/221385
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Kerwin Wan <kerwinw@nvidia.com>
    Reviewed-by: Youngjin Kim <nkim@nvidia.com>
    Reviewed-by: Hayden Du <haydend@nvidia.com>
    Reviewed-by: Matt Wagner <mwagner@nvidia.com>
    Reviewed-by: Min-wuk Lee <mlee@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board-macallan-panel.c
+++ arch/arm/mach-tegra/board-macallan-panel.c
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-tegra/board-macallan-panel.c
  *
- * Copyright (c) 2013, NVIDIA Corporation.
+ * Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -459,6 +459,16 @@ int __init macallan_panel_init(void)
 	__tegra_move_framebuffer(&macallan_nvmap_device,
 		tegra_fb_start, tegra_bootloader_fb_start,
 			min(tegra_fb_size, tegra_bootloader_fb_size));
+	/*
+	 * If the bootloader fb2 is valid, copy it to the fb2, or else
+	 * clear fb2 to avoid garbage on dispaly2.
+	 */
+	if (tegra_bootloader_fb2_size)
+		tegra_move_framebuffer(tegra_fb2_start,
+			tegra_bootloader_fb2_start,
+			min(tegra_fb2_size, tegra_bootloader_fb2_size));
+	else
+		tegra_clear_framebuffer(tegra_fb2_start, tegra_fb2_size);
 
 	res = platform_get_resource_byname(&macallan_disp2_device,
 		IORESOURCE_MEM, "fbmem");
--- arch/arm/mach-tegra/board-tegratab-panel.c
+++ arch/arm/mach-tegra/board-tegratab-panel.c
@@ -471,6 +471,16 @@ int __init tegratab_panel_init(void)
 	__tegra_move_framebuffer(&tegratab_nvmap_device,
 		tegra_fb_start, tegra_bootloader_fb_start,
 			min(tegra_fb_size, tegra_bootloader_fb_size));
+	/*
+	 * If the bootloader fb2 is valid, copy it to the fb2, or else
+	 * clear fb2 to avoid garbage on dispaly2.
+	 */
+	if (tegra_bootloader_fb2_size)
+		tegra_move_framebuffer(tegra_fb2_start,
+			tegra_bootloader_fb2_start,
+			min(tegra_fb2_size, tegra_bootloader_fb2_size));
+	else
+		tegra_clear_framebuffer(tegra_fb2_start, tegra_fb2_size);
 
 	res = platform_get_resource_byname(&tegratab_disp2_device,
 		IORESOURCE_MEM, "fbmem");
