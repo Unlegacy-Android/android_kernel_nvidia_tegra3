cb1267b81ec8651c268d107de452e997d1780933
Rohith Seelaboyina <rseelaboyina@nvidia.com>
2013-04-23 18:44:57 +0530

    ARM: tegra: usb: disable vbus while going to lp0
    disable vbus while going to lp0
    
    Bug 1274508
    
    Change-Id: I1abed5ad250083aab1f06b26ddfbea37e5f0aec4
    Signed-off-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>
    Reviewed-on: http://git-master/r/221578
    Reviewed-by: Automatic_Commit_Validation_User
    Tested-by: Harry Hong <hhong@nvidia.com>
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>

--- arch/arm/mach-tegra/include/mach/usb_phy.h
+++ arch/arm/mach-tegra/include/mach/usb_phy.h
@@ -112,4 +112,6 @@ void tegra_usb_phy_memory_prefetch_on(struct tegra_usb_phy *phy);
 
 void tegra_usb_phy_memory_prefetch_off(struct tegra_usb_phy *phy);
 
+void tegra_usb_enable_vbus(struct tegra_usb_phy *phy, bool enable);
+
 #endif /* __MACH_USB_PHY_H */
