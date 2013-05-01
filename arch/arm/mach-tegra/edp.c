b5bc83fd2eec9925d6147796386cdaf391a42ecb
Alex Frid <afrid@nvidia.com>
2013-05-01 00:40:50 -0700

    ARM: tegra11: power: Increase edp voltage thermal cap
    Since edp maximum voltage constraint is applied on top of CPU CL-DVFS
    Vmax thermal profile, edp limit was re-characterized.
    
    Bug 1270003
    
    Change-Id: Ic8acb18429f89cd554342b1d7230fd49123ecd3a
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/224495
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>
    Tested-by: Diwakar Tundlam <dtundlam@nvidia.com>

--- arch/arm/mach-tegra/edp.c
+++ arch/arm/mach-tegra/edp.c
@@ -377,7 +377,7 @@ static struct tegra_edp_cpu_leakage_params leakage_params[] = {
 			   {   15618709,   -4576116,   158401,  -1538, },
 			 },
 		 },
-		.volt_temp_cap = { 70, 1240 },
+		.volt_temp_cap = { 70, 1300 },
 	},
 	{
 		.cpu_speedo_id	    = 1, /* A01P+ CPU */
@@ -410,7 +410,7 @@ static struct tegra_edp_cpu_leakage_params leakage_params[] = {
 			 },
 		 },
 		.safety_cap = { 1810500, 1810500, 1606500, 1606500 },
-		.volt_temp_cap = { 70, 1240 },
+		.volt_temp_cap = { 70, 1300 },
 	},
 	{
 		.cpu_speedo_id	    = 2, /* A01P+ fast CPU */
@@ -443,7 +443,7 @@ static struct tegra_edp_cpu_leakage_params leakage_params[] = {
 			 },
 		 },
 		.safety_cap = { 1912500, 1912500, 1912500, 1912500 },
-		.volt_temp_cap = { 70, 1240 },
+		.volt_temp_cap = { 70, 1300 },
 	},
 };
 
