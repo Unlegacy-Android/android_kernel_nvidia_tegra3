1c1424344ce90aff60390ac37b20eda02c53b5c8
Chaitanya Bandi <bandik@nvidia.com>
2013-05-03 16:23:22 +0530

    ARM: tegra: pluto: Use battery info from command line
    Use battery presence information from kernel command line
    and indicate through platform data to max77665 charger
    and max17042 fuel guage.
    
    Bug 1281739
    
    Change-Id: Icf6ce390da0fc5d6f3dc2d1a2e1f69fe367d1658
    Signed-off-by: Chaitanya Bandi <bandik@nvidia.com>
    Reviewed-on: http://git-master/r/225220
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- arch/arm/mach-tegra/board-pluto-sensors.c
+++ arch/arm/mach-tegra/board-pluto-sensors.c
@@ -141,7 +141,8 @@ static struct max17042_platform_data max17042_pdata = {
 	.enable_current_sense = 1,
 	.r_sns = 0,
 	.rbat_map = max17042_rbat_map,
-	.edp_client = &bat_depl_client
+	.edp_client = &bat_depl_client,
+	.is_battery_present = false, /* False as default */
 };
 
 static struct i2c_board_info max17042_device[] = {
@@ -263,6 +264,7 @@ static struct max77665_charger_plat_data max77665_charger = {
 	.cables = maxim_cable,
 	.extcon_name = "tegra-udc",
 	.update_status = max17042_update_status,
+	.is_battery_present = false, /* false as default */
 };
 
 static struct max77665_muic_platform_data max77665_muic = {
@@ -1163,6 +1165,10 @@ void __init max77665_init(void)
 {
 	int err;
 
+	/* For battery presence into charger driver */
+	if (get_power_supply_type() == POWER_SUPPLY_TYPE_BATTERY)
+		max77665_charger.is_battery_present = true;
+
 	err = i2c_register_board_info(4, pluto_i2c_board_info_max77665,
 		ARRAY_SIZE(pluto_i2c_board_info_max77665));
 	if (err)
@@ -1191,6 +1197,9 @@ int __init pluto_sensors_init(void)
 
 	mpuirq_init();
 	max77665_init();
+
+	if (get_power_supply_type() == POWER_SUPPLY_TYPE_BATTERY)
+		max17042_pdata.is_battery_present = true;
 
 	err = i2c_register_board_info(0, max17042_device,
 				ARRAY_SIZE(max17042_device));
