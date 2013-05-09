85c49d81cd5c6760567a44878fedbc09c422841d
Rohith Seelaboyina <rseelaboyina@nvidia.com>
2013-05-09 09:53:23 +0530

    usb: host: tegra: boost cpu frequency when bus is active
    Boost cpu frequency (based upon TEGRA_EHCI_BOOST_CPU_FREQ)
    when the usb bus is active and remove the boost when usb
    bus is suspended.
    
    Bug 1266414
    
    Change-Id: Ic5b7b1ecc923caf7da458383112ed0e647448a57
    Signed-off-by: Rohith Seelaboyina <rseelaboyina@nvidia.com>
    Reviewed-on: http://git-master/r/226914
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>

--- drivers/usb/host/ehci-tegra.c
+++ drivers/usb/host/ehci-tegra.c
@@ -26,6 +26,7 @@
 
 #include <mach/usb_phy.h>
 #include <mach/iomap.h>
+#include <linux/pm_qos.h>
 
 #if 0
 #define EHCI_DBG(stuff...)	pr_info("ehci-tegra: " stuff)
@@ -56,6 +57,11 @@ struct tegra_ehci_hcd {
 	bool bus_suspended_fail;
 	bool unaligned_dma_buf_supported;
 	bool has_hostpc;
+#ifdef CONFIG_TEGRA_EHCI_BOOST_CPU_FREQ
+	bool cpu_boost_in_work;
+	struct delayed_work boost_cpu_freq_work;
+	struct pm_qos_request boost_cpu_freq_req;
+#endif
 };
 
 struct dma_align_buffer {
@@ -152,6 +158,17 @@ static void tegra_ehci_unmap_urb_for_dma(struct usb_hcd *hcd,
 	usb_hcd_unmap_urb_for_dma(hcd, urb);
 	free_align_buffer(urb, hcd);
 }
+
+#ifdef CONFIG_TEGRA_EHCI_BOOST_CPU_FREQ
+static void tegra_ehci_boost_cpu_frequency_work(struct work_struct *work)
+{
+	struct tegra_ehci_hcd *tegra = container_of(work,
+		struct tegra_ehci_hcd, boost_cpu_freq_work.work);
+	if (tegra->cpu_boost_in_work)
+		pm_qos_update_request(&tegra->boost_cpu_freq_req,
+		(s32)CONFIG_TEGRA_EHCI_BOOST_CPU_FREQ * 1000);
+}
+#endif
 
 static irqreturn_t tegra_ehci_irq(struct usb_hcd *hcd)
 {
@@ -367,6 +384,13 @@ static int tegra_ehci_bus_suspend(struct usb_hcd *hcd)
 	struct tegra_ehci_hcd *tegra = dev_get_drvdata(hcd->self.controller);
 	int err = 0;
 	EHCI_DBG("%s() BEGIN\n", __func__);
+
+#ifdef CONFIG_TEGRA_EHCI_BOOST_CPU_FREQ
+	pm_qos_update_request(&tegra->boost_cpu_freq_req,
+			PM_QOS_DEFAULT_VALUE);
+	tegra->cpu_boost_in_work = false;
+#endif
+
 	mutex_lock(&tegra->sync_lock);
 	tegra->bus_suspended_fail = false;
 	err = ehci_bus_suspend(hcd);
@@ -385,6 +409,13 @@ static int tegra_ehci_bus_resume(struct usb_hcd *hcd)
 	struct tegra_ehci_hcd *tegra = dev_get_drvdata(hcd->self.controller);
 	int err = 0;
 	EHCI_DBG("%s() BEGIN\n", __func__);
+
+#ifdef CONFIG_TEGRA_EHCI_BOOST_CPU_FREQ
+	pm_qos_update_request(&tegra->boost_cpu_freq_req,
+			(s32)CONFIG_TEGRA_EHCI_BOOST_CPU_FREQ * 1000);
+	tegra->cpu_boost_in_work = false;
+
+#endif
 
 	mutex_lock(&tegra->sync_lock);
 	usb_phy_set_suspend(get_usb_phy(tegra->phy), 0);
@@ -585,6 +616,16 @@ static int tegra_ehci_probe(struct platform_device *pdev)
 			otg_set_host(tegra->transceiver->otg, &hcd->self);
 	}
 #endif
+
+#ifdef CONFIG_TEGRA_EHCI_BOOST_CPU_FREQ
+	INIT_DELAYED_WORK(&tegra->boost_cpu_freq_work,
+					tegra_ehci_boost_cpu_frequency_work);
+	pm_qos_add_request(&tegra->boost_cpu_freq_req, PM_QOS_CPU_FREQ_MIN,
+					PM_QOS_DEFAULT_VALUE);
+	schedule_delayed_work(&tegra->boost_cpu_freq_work, 4000);
+	tegra->cpu_boost_in_work = true;
+#endif
+
 	return err;
 
 fail_phy:
@@ -637,6 +678,13 @@ static int tegra_ehci_remove(struct platform_device *pdev)
 
 	if (hcd == NULL)
 		return -EINVAL;
+
+#ifdef CONFIG_TEGRA_EHCI_BOOST_CPU_FREQ
+	cancel_delayed_work(&tegra->boost_cpu_freq_work);
+	pm_qos_update_request(&tegra->boost_cpu_freq_req,
+				PM_QOS_DEFAULT_VALUE);
+	tegra->cpu_boost_in_work = false;
+#endif
 
 #ifdef CONFIG_USB_OTG_UTILS
 	if (tegra->transceiver) {
