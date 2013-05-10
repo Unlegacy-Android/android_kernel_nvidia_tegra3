3a9a094dd41ad27451c1d989159adc074ec7f73a
Tony Liu <tonliu@nvidia.com>
2013-05-10 10:12:32 +0000

    arm: tegra: macallan: set hpa02149 to support oc alert
    -set min_core_online to support oc alert when cpus_online is over this value
    -set current_threshold to generate oc alert when current is over this value
    
    Bug 1255452
    
    Change-Id: I9cf760d029e63f5df84222fc01ab5544e5d26086
    Signed-off-by: Tony Liu <tonliu@nvidia.com>
    Reviewed-on: http://git-master/r/227413
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Puneet Saxena <puneets@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Deepak Nibade <dnibade@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- arch/arm/mach-tegra/board-macallan-powermon.c
+++ arch/arm/mach-tegra/board-macallan-powermon.c
@@ -233,9 +233,12 @@ static struct ina230_platform_data power_mon_ina230_info[] = {
 		.calibration_data  = 0x20c4,
 		.power_lsb = 3.051757813 * PRECISION_MULTIPLIER_MACALLAN,
 		.rail_name = "VDD_CELL",
-		.resistor = -5,
+		.resistor = 5,
+		.min_cores_online = 2,
 		.divisor = 25,
 		.precision_multiplier = PRECISION_MULTIPLIER_MACALLAN,
+		/* set to 5A, wait for syseng tuning */
+		.current_threshold = 5000,
 		.shunt_polarity_inverted = 1,
 	}
 };
