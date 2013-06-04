a7db3134b3e99e7e7d0d3e344738d0dddb6d781b
Marvin Zhang <mzhang@nvidia.com>
2013-06-04 16:56:25 -0700

    Revert "hdmi: don't call tegra_dc_hpd in spinlock"
    Bug 1298680
    
    This reverts commit 946e53410468dafd92b6ccb313dba1bb1b72d04d.
    
    Change-Id: Iccea1de23790c6cb4b53022bc7f657b8b600e64c
    Signed-off-by: Marvin Zhang <mzhang@nvidia.com>
    Reviewed-on: http://git-master/r/235653
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Karan Jhavar <kjhavar@nvidia.com>
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- drivers/video/tegra/dc/hdmi.c
+++ drivers/video/tegra/dc/hdmi.c
@@ -956,8 +956,12 @@ static irqreturn_t tegra_dc_hdmi_irq(int irq, void *ptr)
 	spin_lock_irqsave(&hdmi->suspend_lock, flags);
 	if (!hdmi->suspended) {
 		__cancel_delayed_work(&hdmi->work);
-		queue_delayed_work(system_nrt_wq, &hdmi->work,
-				   msecs_to_jiffies(100));
+		if (tegra_dc_hdmi_hpd(dc))
+			queue_delayed_work(system_nrt_wq, &hdmi->work,
+					   msecs_to_jiffies(100));
+		else
+			queue_delayed_work(system_nrt_wq, &hdmi->work,
+					   msecs_to_jiffies(30));
 	}
 	spin_unlock_irqrestore(&hdmi->suspend_lock, flags);
 
