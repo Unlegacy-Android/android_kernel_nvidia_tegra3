67959cb39ee7dab94310563518e9e2b94e7ac16b
PJBrs <pjbrs@floorenpj.xs4all.nl>
2017-03-31 21:11:06 +0200

    Add s5k5cag camera driver
    Change-Id: Ic6f130cc8ff0fc020754fb692cdb7a4b31eb0b38

---
+++ include/media/s5k5cag.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2012 NVIDIA Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA
+ */
+
+#ifndef __S5K5CAG_H__
+#define __S5K5CAG_H__
+
+#define SENSOR_NAME	"s5k5cag"
+#define DEV(x)		"/dev/"x
+#define SENSOR_PATH	DEV(SENSOR_NAME)
+#define LOG_NAME(x)	"ImagerODM-"x
+#define LOG_TAG		LOG_NAME(SENSOR_NAME)
+
+#endif  /* __S5K5CAG_H__ */
+
