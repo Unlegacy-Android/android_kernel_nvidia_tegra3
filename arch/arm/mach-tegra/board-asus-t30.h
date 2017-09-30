700814671823fa9ee073cb45f8ff2a92264a85da
Svyatoslav <clamor95@gmail.com>
2017-09-30 19:02:35 +0300

    asus-t30: update machine init
    Squashed commit of:
    * ea402c7 - soc device registration
    * f38c4a2 - Registered tegra-fuse device in T30 board files
    * abf2d41 - separate pinmux and gpio init
    * d17d750 - register pinmux and gpio devices before pinmux init
    * b4620d3 - Create IOMMU map at board_init w/ PLATFORM_IOMMUABLE
    * e503a08 - move tegra_camera
    * 763294a - TMR8/TMR9 not to be used for watchdog
    * 59c0680 - add support for persistent_ram
    
    Change-Id: I56ab49a366f349ec2dd53860ec7658c635d0f20e

--- arch/arm/mach-tegra/board-asus-t30.h
+++ arch/arm/mach-tegra/board-asus-t30.h
@@ -224,6 +224,7 @@ int cardhu_regulator_init(void);
 int cardhu_suspend_init(void);
 int cardhu_sdhci_init(void);
 int cardhu_pinmux_init(void);
+int cardhu_gpio_init(void);
 int cardhu_panel_init(void);
 int cardhu_sensors_init(void);
 int cardhu_keys_init(void);
