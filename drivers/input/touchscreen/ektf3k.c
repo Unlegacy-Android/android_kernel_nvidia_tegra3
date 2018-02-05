2029e958ac03611b851d7c54d7942be50d0e3e33
Svyatoslav Ryhel <clamor95@gmail.com>
2018-02-05 14:11:22 +0200

    ektf3k: adapt for TF300/TF700
    Generic elan driver works fine with TF touchscreens with slight update.
    Also add TF calibration firmware.
    
    Change-Id: I2b2956c47f5f388d86753637190179a25286062f
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- drivers/input/touchscreen/ektf3k.c
+++ drivers/input/touchscreen/ektf3k.c
@@ -48,9 +48,15 @@
 
 #define ABS_MT_POSITION		0x2a /* Group a set of X and Y */
 
+#ifdef CONFIG_MACH_TRANSFORMER
+#define FIRMWARE_UPDATE_WITH_HEADER 0 
+#define FIRMWARE_NAME "elan/02-3011-4820.ekt"
+MODULE_FIRMWARE(FIRMWARE_NAME);
+#else
 #define FIRMWARE_UPDATE_WITH_HEADER 1 
 #define FIRMWARE_NAME "elan/ektf3k.fw"
 MODULE_FIRMWARE(FIRMWARE_NAME);
+#endif
 
 static uint8_t firmware_recovery = 0x00;
 static uint8_t work_lock = 0;
@@ -478,14 +484,23 @@ static void ektf3k_ts_report_data(struct i2c_client *client, uint8_t *buf)
 				input_mt_report_slot_state(ts->input_dev, MT_TOOL_FINGER, active);
 				if (active) {
 					ektf3k_ts_parse_xy(&buf[idx], &x, &y);
+#ifdef CONFIG_MACH_TRANSFORMER
+					y = ts->abs_y_max - y;
+#else
 					x = x > ts->abs_x_max ? 0 : ts->abs_x_max - x;
 					y = y > ts->abs_y_max ? ts->abs_y_max : y; 
+#endif
 					touch_size = ((i & 0x01) ? buf[size_index[i]] : (buf[size_index[i]] >> 4)) & 0x0F;
 					pressure_size = touch_size << 4; // max pressure value is 255
 					input_report_abs(idev, ABS_MT_TOUCH_MAJOR, touch_size);
 					input_report_abs(idev, ABS_MT_PRESSURE, pressure_size);
+#ifdef CONFIG_MACH_TRANSFORMER
+					input_report_abs(idev, ABS_MT_POSITION_X, x);
+					input_report_abs(idev, ABS_MT_POSITION_Y, y);
+#else
 					input_report_abs(idev, ABS_MT_POSITION_X, y);
 					input_report_abs(idev, ABS_MT_POSITION_Y, x);
+#endif
 				}
 			}
 			mTouchStatus[i] = active;
@@ -904,8 +919,13 @@ static int ektf3k_ts_probe(struct i2c_client *client,
 	ts->abs_y_max = pdata->abs_y_max;
 
 	input_mt_init_slots(ts->input_dev, FINGER_NUM);
-	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_X, pdata->abs_y_min,pdata->abs_y_max, 0, 0);
-	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_Y, pdata->abs_x_min,pdata->abs_x_max, 0, 0);
+#ifdef CONFIG_MACH_TRANSFORMER
+	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_X, pdata->abs_x_min, pdata->abs_x_max, 0, 0);
+	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_Y, pdata->abs_y_min, pdata->abs_y_max, 0, 0);
+#else
+	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_X, pdata->abs_y_min, pdata->abs_y_max, 0, 0);
+	input_set_abs_params(ts->input_dev, ABS_MT_POSITION_Y, pdata->abs_x_min, pdata->abs_x_max, 0, 0);
+#endif
 	input_set_abs_params(ts->input_dev, ABS_MT_TOUCH_MAJOR, 0, MAX_FINGER_SIZE, 0, 0);
 	input_set_abs_params(ts->input_dev, ABS_MT_PRESSURE, 0, MAX_FINGER_PRESSURE, 0, 0);
 
